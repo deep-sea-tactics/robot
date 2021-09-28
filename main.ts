9acc6e14d607ac6d8c98daaf359c008cf0f7fbe3
LeoDog896 <LeoDog896@hotmail.com>
2021-09-28 19:29:19 -0400

    Use ABC, cleanup

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
-import { Application, send } from "https://deno.land/x/oak/mod.ts";
-import * as log from "https://deno.land/std@0.106.0/log/mod.ts";
+import { Application } from "abc";
+import * as log from "log";
+import { open } from 'open';
 
 const app = new Application();
 
-app.use(async (context) => {
-	await send(context, context.request.url.pathname, {
-		root: `${Deno.cwd()}/client/public`,
-		index: "index.html",
-	});
-});
-
 log.info("Server started on http://localhost:3000");
 
-await app.listen({ port: 3000 });
+app
+	.file("/", "./client/public/index.html")
+	.static("/", "./client/public")
+	.start({ port: 3000 })
+
+await open("https://localhost:3000");
