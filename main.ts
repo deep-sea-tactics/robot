e6b52a976b2b1b970e3bf3477ff16e2c79e7122a
LeoDog896 <LeoDog896@hotmail.com>
2021-09-28 16:14:44 -0400

    Add logging

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 import { Application, send } from "https://deno.land/x/oak/mod.ts";
+import * as log from "https://deno.land/std@0.106.0/log/mod.ts";
 
 const app = new Application();
 
 app.use(async (context) => {
 	await send(context, context.request.url.pathname, {
-	  root: `${Deno.cwd()}/public`,
-	  index: "index.html",
+		root: `${Deno.cwd()}/public`,
+		index: "index.html",
 	});
-  });
+});
+
+log.info("Server started on http://localhost:3000");
+
 await app.listen({ port: 3000 });
