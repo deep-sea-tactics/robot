3fae81cae69a3b4d196962d09ff19f35e206d28c
LeoDog896 <LeoDog896@hotmail.com>
2021-09-29 14:48:31 +0000

    More drawing

--- client/src/controller/ControllerCanvas.svelte
+++ client/src/controller/ControllerCanvas.svelte
@@ -1,26 +1,46 @@
 <script lang="ts">
 
-	import { Canvas, Layer, t, getCanvas } from "svelte-canvas";
+	import { Canvas, Layer } from "svelte-canvas";
 
-	interface MousePos {
+	interface Position {
 		x: number;
 		y: number;
 	}
 
-	let canvas: Canvas
-	let mousePos: MousePos = { x: 0, y: 0 }
+	interface RenderInterface {
+		context: CanvasRenderingContext2D
+		width: number;
+		height: number;
+	}
 
-	$: render = ({ context, width, height }) => {
+	const mouseRadius = 10
+	let canvas: Canvas
+	let mousePos: Position = { x: canvas.width / 2, y: canvas.height / 2 }
+
+	$: render = ({ context, width, height }: RenderInterface) => {
+
+		const canvasOrigin: Position = {
+			x: width / 2,
+			y: height / 2
+		}
+
 		// Store the circle radius, useful for restricting the cursor
 		const circleRadius = width / 2 - 10;
 
 		// Create the circle (Last two arguments are in Radians)
 		context.beginPath();
-		context.arc(width / 2, height / 2, circleRadius, 0, 2 * Math.PI);
+		context.arc(canvasOrigin.x, canvasOrigin.y, circleRadius, 0, 2 * Math.PI);
 		context.stroke();
 
+		// Create a circle around the mouse
 		context.beginPath();
-		context.arc(mousePos.x, mousePos.y, 10, 0, 2 * Math.PI);
+		context.arc(mousePos.x, mousePos.y, mouseRadius, 0, 2 * Math.PI);
+		context.stroke();
+
+		// Create a line between the center of the circle and the mouse's position
+		context.beginPath();
+		context.moveTo(canvasOrigin.x, canvasOrigin.y)
+		context.lineTo(mousePos.x, mousePos.y);
 		context.stroke();
 	}
 
@@ -38,6 +58,8 @@
 	<Canvas bind:this={canvas} on:mousemove={mouseEvent} width={300} height={300}>
 		<Layer {render}></Layer>
 	</Canvas>
+	<button>Use Controller</button>
+	<button>Use Mouse</button>
 </div>
 
 <style lang="scss">
