c2996c729318ea7435a63bf0e265b4d9007819e9
LeoDog896 <LeoDog896@hotmail.com>
2021-09-28 20:15:17 -0400

    Begin binding to HID

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,19 @@ import express from "express";
 import cors from "cors";
 import * as HID from "node-hid";
 
-console.log(HID.devices());
+function findController(): HID.HID | undefined {
+	const foundDevices = HID.devices().filter(item => item.product == "Logitech Extreme 3D");
+
+	if (foundDevices.length === 0) return undefined
+
+	return new HID.HID(foundDevices[0].path as string);
+}
+
+const device = findController() as HID.HID;
+
+device.on("data", function(data) {
+	console.log(data)
+});
 
 const app = express();
 const port = 3000;
