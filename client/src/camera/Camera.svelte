9066407d7fc612bf47ff868a5ad714ddd23f2975
LeoDog896 <LeoDog896@hotmail.com>
2021-10-19 18:20:56 +0000

    Replace with iframe

--- client/src/camera/Camera.svelte
+++ client/src/camera/Camera.svelte
@@ -1,13 +1,23 @@
 <div id="camera">
-	<p>Camera Element</p>
+	<iframe title="Camera" src="https://google.com"></iframe>
 </div>
 <style lang="scss">
 	#camera {
 		background-color: gray;
 		width: 50%;
 		height: 50%;
+		padding: 0;
+		margin: 0;
 		flex-grow: 1;
 		text-align: center;
 		color: white;
+
+		iframe {
+			width: 100%;
+			height: 100%;
+			padding: 0px;
+			border: none;
+			margin: 0px;
+		}
 	}
 </style>
