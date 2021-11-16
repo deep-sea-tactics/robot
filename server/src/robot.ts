570c616c40cf81420849a2ebed345a199aae67f4
LeoDog896 <LeoDog896@hotmail.com>
2021-11-16 14:28:18 -0500

    Handle timeout error

--- server/src/robot.ts
+++ server/src/robot.ts
@@ -4,6 +4,23 @@ import { logger } from './logger'
 const ip = "192.168.1.201";
 const port = 9000
 
+let hasConnected = false
+
 const socket = net.createConnection(port, ip, () => {
-    logger.info("Connected!")
+    logger.info("Connected to robot!")
+    hasConnected = true
+})
+
+socket.on("connect", () => {
+    if (hasConnected)
+        logger.info("Reconnected to robot.")
+})
+
+socket.on("error", e => logger.warn(e))
+
+socket.on("close", () => {
+    logger.warn("Disconnected; Attempting to reconnect")
+    socket.setTimeout(1000, () => {
+        socket.connect(port, ip)
+    });
 })
