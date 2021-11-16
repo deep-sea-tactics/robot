9ff32f4c568c15878ad601939ee0755e9032febe
LeoDog896 <LeoDog896@hotmail.com>
2021-11-16 15:56:51 -0500

    generate and better constructor

--- packet/src/packet.ts
+++ packet/src/packet.ts
@@ -1,4 +1,18 @@
-export interface Packet {
-    id: number;
-    data: string | Uint8Array;
+export abstract class Packet {
+    abstract id: number;
+    data: Uint8Array;
+
+    constructor(data: Uint8Array) {
+        this.data = data
+    }
+
+    /** Creates the Uint8Array from this packet */
+    generate(): Uint8Array {
+        const buildingArray = new Uint8Array(this.data.length + 1)
+        buildingArray[0] = this.id
+        for (let i = 0; i < this.data.length; i++) {
+            buildingArray[i + 1] = this.data[i]
+        }
+        return buildingArray
+    }
 }
