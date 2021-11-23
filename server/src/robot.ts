cedb7b703889929565bfede6d0c901792385f62c
LeoDog896 <LeoDog896@hotmail.com>
2021-11-23 17:33:56 +0000

    Convert to socket.io

--- server/src/robot.ts
+++ server/src/robot.ts
@@ -1,30 +1,17 @@
-import net from 'net'
+import { Server } from "socket.io";
 import { logger } from './logger'
 import flyd from 'flyd'
 import { position } from './control/position';
 
-const ip = "192.168.1.201";
 const port = 9000
 
-let hasConnected = false
+const io = new Server(port);
 
-const socket = net.createConnection(port, ip, () => {
-    logger.info("Connected to robot!")
-    hasConnected = true
-})
+flyd.on(change => io.emit("position", JSON.stringify(change)), position)
 
-flyd.on(change => {
-    socket.write(JSON.stringify(change))
-}, position)
+io.on("connection", socket => {
+    logger.info("Robot connected!")
 
-socket.on("connect", () => {
-    if (hasConnected)
-        logger.info("Reconnected to robot.")
-})
-
-socket.on("error", e => logger.warn(e))
-
-socket.on("close", () => {
-    logger.warn("Disconnected; Attempting to reconnect")
-    setTimeout(() => socket.connect(port, ip), 1000);
+	socket.on("close", () => logger.warn("Robot disconected."))
+	socket.on("error", error => logger.warn("An exception with the robot has occured: " + error))
 })
