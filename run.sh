b189438530f0a14f137850eba126334c4f10f91d
LeoDog896 <LeoDog896@hotmail.com>
2021-12-07 19:55:02 +0000

    run robot!

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 (cd client; pnpm run dev) &
-(cd server; pnpm run start) &
-(python3 2021_CODE/python/V4/Final\ Code/RoverServo.py)
+(cd server; pnpm run start)
 
 fuser -k 5000/tcp # kill the svelte process
---
+++ run_robot.sh
@@ -0,0 +1,4 @@
+(cd robot; pnpm run start) &
+(python3 2021_CODE/python/V4/Final\ Code/RoverServo.py)
+
+fuser -k 9000/tcp # kill the robot process
