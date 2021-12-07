34ee1dc2bcaa4dab7d979f612c55938e46203a83
LeoDog896 <LeoDog896@hotmail.com>
2021-12-07 20:49:55 +0000

    Fix file

--- run_robot.sh
+++ run_robot.sh
@@ -1,4 +1,4 @@
 (cd robot; pnpm run start) &
-(python3 2021_CODE/python/V4/Final\ Code/RoverServo.py)
+(python3 2021_Code/python/V4/Final\ Code/RoverServo.py)
 
 fuser -k 9000/tcp # kill the robot process
