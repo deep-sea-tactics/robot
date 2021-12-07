9d42512def3f2107dc16efe88ab8c0be0173e7f4
LeoDog896 <LeoDog896@hotmail.com>
2021-12-07 19:49:20 +0000

    Add rover servo to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 (cd client; pnpm run dev) &
-(cd server; pnpm run start)
+(cd server; pnpm run start) &
+(python3 2021_CODE/python/V4/Final\ Code/RoverServo.py)
 
 fuser -k 5000/tcp # kill the svelte process
