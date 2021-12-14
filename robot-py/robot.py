c1e2fe4b47bb4e2a4a9ae00ea4848aa19ba91abe
LeoDog896 <LeoDog896@hotmail.com>
2021-12-14 20:49:53 +0000

    More event hooks

--- robot-py/robot.py
+++ robot-py/robot.py
@@ -9,4 +9,16 @@ sio.connect("http://192.168.1.202")
 def on_message(data):
     print(data)
 
+@sio.event
+def connect():
+    print("Robot connected!")
+
+@sio.event
+def connect_error(data):
+    print("Robot connection error: ", data)
+
+@sio.event
+def disconnect():
+    print("Robot disconnected.")
+
 print('Robot connected. SID: ', sio.sid)
