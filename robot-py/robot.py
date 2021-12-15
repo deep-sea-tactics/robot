6a54b3f22f3082e60a3543c5f847aa4e94e6a6cf
Daniel Tomov <daniel308712@gmail.com>
2021-12-14 21:31:57 -0500

    Added python code for servos

--- robot-py/robot.py
+++ robot-py/robot.py
@@ -1,13 +1,22 @@
 import socketio
+import RoverServo as servo
+
 
 # standard Python
 sio = socketio.Client()
 
-sio.connect("http://192.168.1.202:9000")
+sio.connect("http://192.168.1.203:9000")
 
 @sio.on('position')
 def on_message(data):
-    print(data)
+#    print(data)
+    newData=data.split(",")
+    y=newData[1]
+    x=newData[0]
+    newY=float(y[4:-1])
+    newX=float(x[5:-1])
+    print(str(newX) + " " + str(newY))
+    servo.decreaseServo1()
 
 @sio.event
 def connect():
