b63051b9a86584d41bacd20e29ed1fbc8fb83f5e
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2021-12-21 18:26:11 -0500

    Change pigpio import

--- robot-py/RoverESC.py
+++ robot-py/RoverESC.py
@@ -2,8 +2,8 @@
 # -----------------------------------------
 import os     #importing os library so as to communicate with the system
 import time   #importing time library to make Rpi wait because its too impatient 
-os.system ("sudo pigpiod") #Launching GPIO library
-time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
+#os.system ("sudo pigpiod") #Launching GPIO library
+#time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
 import pigpio #importing GPIO library
 
 # -----------------------------------------
