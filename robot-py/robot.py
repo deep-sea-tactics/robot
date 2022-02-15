668b8230de6110e9bc52bff893a4e3cdb5125184
Daniel Tomov <daniel308712@gmail.com>
2022-02-15 15:04:25 -0500

    webrtc

--- robot-py/robot.py
+++ robot-py/robot.py
@@ -3,6 +3,9 @@ import os
 import time
 import json
 
+
+os.system("sudo pigpiod")
+time.sleep(1)
 import pigpio
 
 
---
+++ webrtc/camera2.sh
@@ -0,0 +1,1 @@
+sudo uv4l --external-driver --device-name=video0 --server-option '--port=8082' --server-option '--webrtc-video-format=5'
