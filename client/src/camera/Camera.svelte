7f36312e66cafb009f3e2c7b255a2bf1de12b3f8
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-05-03 15:24:32 -0400

    fixup

--- client/src/camera/Camera.svelte
+++ client/src/camera/Camera.svelte
@@ -1,6 +1,197 @@
+
 <script lang="ts">
-	export let src = "https://google.com";
+	import { onMount } from "svelte";
+
+	export let port: number
+
+	const signalling_server_address = "192.168.1.201:" + port;
+	const pcConfig = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]};
+	const mediaConstraints = {
+		optional: [],
+		mandatory: {
+			OfferToReceiveVideo: true
+		}
+	};
+	let startButton: HTMLButtonElement
+	let stopButton: HTMLButtonElement
+	let video: HTMLVideoElement
+	let ws;
+	let iceCandidates = []
+	let remoteDesc;
+	let pc;
+
+	function addIceCandidates() {
+		iceCandidates.forEach(function (candidate) {
+			pc.addIceCandidate(candidate, () => void 0,
+				function (error) {
+					console.error("addIceCandidate error: " + error);
+				}
+			);
+		});
+		iceCandidates = [];
+	}
+
+	function onTrack(event) {
+		console.log("Remote track!");
+		video.srcObject = event.streams[0];
+	}
+
+	function onRemoteStreamRemoved(event) {
+		video.srcObject = null;
+		video.src = ''; // TODO: remove
+	}
+
+	function onIceCandidate(event) {
+		if (event.candidate && event.candidate.candidate) {
+			const candidate = {
+				sdpMLineIndex: event.candidate.sdpMLineIndex,
+				sdpMid: event.candidate.sdpMid,
+				candidate: event.candidate.candidate
+			};
+			const request = {
+				what: "addIceCandidate",
+				data: JSON.stringify(candidate)
+			};
+			ws.send(JSON.stringify(request));
+		} else {
+			console.log("End of candidates.");
+		}
+	}
+
+	function createPeerConnection() {
+		try {
+			pc = new RTCPeerConnection(pcConfig);
+			pc.onicecandidate = onIceCandidate;
+			pc.ontrack = onTrack;
+			pc.onremovestream = onRemoteStreamRemoved;
+		} catch (e) {
+			console.error("createPeerConnection() failed");
+		}
+	}
+
+	function start() {
+		stopButton.disabled = false;
+		startButton.disabled = true;
+		document.documentElement.style.cursor = 'wait';
+
+		const protocol = location.protocol === "https:" ? "wss:" : "ws:";
+		ws = new WebSocket(protocol + '//' + signalling_server_address + '/stream/webrtc');
+
+		ws.onopen = function () {
+			iceCandidates = [];
+			remoteDesc = false;
+			createPeerConnection();
+			var request = {
+				what: "call",
+				options: {
+					trickle_ice: true
+				}
+			};
+			ws.send(JSON.stringify(request));
+		};
+
+		ws.onmessage = function (evt) {
+			var msg = JSON.parse(evt.data);
+			if (msg.what === "undefined") return
+			const { what, data } = msg
+
+			switch (what) {
+				case "offer":
+					pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(data)),
+							function onRemoteSdpSuccess() {
+								remoteDesc = true;
+								addIceCandidates();
+								pc.createAnswer(function (sessionDescription) {
+									pc.setLocalDescription(sessionDescription);
+									const request = {
+										what: "answer",
+										data: JSON.stringify(sessionDescription)
+									};
+									ws.send(JSON.stringify(request));
+
+								}, function (error) {
+									alert("Failed to createAnswer: " + error);
+
+								}, mediaConstraints);
+							},
+							function onRemoteSdpError(event) {
+								alert('Failed to set remote description (unsupported codec on this browser?): ' + event);
+								stop();
+							}
+					);
+					
+					break;
+
+				case "iceCandidate": // when trickle is enabled
+					if (!msg.data) {
+						console.log("Ice Gathering Complete");
+						break;
+					}
+					var elt = JSON.parse(msg.data);
+					let candidate = new RTCIceCandidate({sdpMLineIndex: elt.sdpMLineIndex, candidate: elt.candidate});
+					iceCandidates.push(candidate);
+					if (remoteDesc)
+						addIceCandidates();
+					document.documentElement.style.cursor = 'default';
+					break;
+			}
+		};
+
+		ws.onclose = function (evt) {
+			if (pc) {
+				pc.close();
+				pc = null;
+			}
+			stopButton.disabled = true;
+			startButton.disabled = false;
+			document.documentElement.style.cursor = 'default';
+		};
+
+		ws.onerror = function (evt) {
+			alert("An error has occurred!" + evt);
+			ws.close();
+		};
+	}
+
+	function stop() {
+		video.srcObject = null;
+		video.src = ''; // TODO; remove
+		if (pc) {
+			pc.close();
+			pc = null;
+		}
+		if (ws) {
+			ws.close();
+			ws = null;
+		}
+		stopButton.disabled = true;
+		startButton.disabled = false;
+		document.documentElement.style.cursor = 'default';
+		console.log(1)
+	}
+
+	onMount(() => {
+		window.onbeforeunload = function () {
+			if (ws) {
+				ws.onclose = function () {}; // disable onclose handler first
+				stop();
+			}
+		};
+	});
 </script>
-<div id="camera" class="bg-gray-400 w-1/2 h-full p-0 m-0 grow text-white text-center">
-	<iframe class="w-full h-full p-0 m-0 border-0" title="Camera" {src}></iframe>
+<style>
+	video {
+		border: 1px solid #aaa;
+	}
+</style>
+<div class="overlayWrapper">
+	<video bind:this={video} id="remote-video" autoplay width="640" height="480">
+		<track kind="captions"/>
+		Your browser does not support the video tag.
+	</video>
 </div>
+<div id="controls">
+	<button id="start" bind:this={startButton} style="background-color: green;" on:click={start}>Start</button>
+	<button disabled id="stop" bind:this={stopButton} style="background-color: red" on:click={stop}>Stop</button>
+</div>
+<br>
--- client/src/camera/CameraBox.svelte
+++ client/src/camera/CameraBox.svelte
@@ -2,6 +2,5 @@
 	import Camera from "./Camera.svelte"
 </script>
 <div id="cameras" class="w-2/3 h-screen flex flex-wrap bg-gray-100">
-	<Camera src="file:///home/daniel/LDST/Landstown-Robotics-Challenge/client/src/raw.html#8081"/>
-	<Camera src="file:///home/daniel/LDST/Landstown-Robotics-Challenge/client/src/raw.html#8082"/>
+	<Camera port={8081}/>
 </div>
