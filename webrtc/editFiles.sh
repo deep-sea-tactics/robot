f8219c0256491425acd0d573b4a12115f5e6b1f9
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-05-14 00:37:13 -0400

    Update editFiles.sh

--- webrtc/editFiles.sh
+++ webrtc/editFiles.sh
@@ -1,8 +1,17 @@
 #top
-=$(v4l2-ctl --list-devices 2>/dev/null  | tac | sed -n '/HD USB/q;p' | tail -1 | sed 's/\t//')
+top=$(v4l2-ctl --list-devices 2>/dev/null  | tac | sed -n '/HD USB/q;p' | tail -1 | sed 's/\t//')
 
 #front
-variable=$(v4l2-ctl --list-devices 2>/dev/null | tac | sed -n '/mmal/q;p' | tail -1 | sed 's/\t//')
+front=$(v4l2-ctl --list-devices 2>/dev/null | tac | sed -n '/mmal/q;p' | tail -1 | sed 's/\t//')
 
 #bottom
-variable=$(v4l2-ctl --list-devices 2>/dev/null | tac | sed -n '/H264/q;p' | tail -1 | sed 's/\t//')
+bottom=$(v4l2-ctl --list-devices 2>/dev/null | tac | sed -n '/H264/q;p' | tail -1 | sed 's/\t//')
+
+#front
+echo "sudo uv4l --external-driver --device-name=video6 --server-option '--port=8081' --server-option '--webrtc-video-format=5'" > camera1.sh.test
+
+#front
+echo "sudo uv4l --external-driver --device-name=video6 --server-option '--port=8081' --server-option '--webrtc-video-format=5'" > camera2.sh.test
+
+#front
+echo "sudo uv4l --external-driver --device-name=video6 --server-option '--port=8081' --server-option '--webrtc-video-format=5'" > camera3.sh.test
