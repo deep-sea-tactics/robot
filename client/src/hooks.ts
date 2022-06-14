f79a840eed7cd0903fe73cd15131e61040c5ef69
LeoDog896 <LeoDog896@hotmail.com>
2022-06-14 20:26:25 +0000

    quirky little hooks

--- client/src/hooks.ts
+++ client/src/hooks.ts
@@ -1,5 +1,8 @@
 import type { Handle } from "@sveltejs/kit"
 
 export const handle: Handle = async ({ event, resolve }) => {
-  return resolve(event, { ssr: false });
+  const response = await resolve(event, { ssr: false });
+  response.headers.set('Access-Control-Allow-Origin', event.url.origin);
+ 
+  return response;
 }
