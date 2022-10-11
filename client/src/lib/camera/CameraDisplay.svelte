bbae21917162966b932ffd68b7cd224ae849809c
LeoDog896 <LeoDog896@hotmail.com>
2022-10-11 19:12:25 +0000

    camera preprocess!!!

--- client/src/lib/camera/CameraDisplay.svelte
+++ client/src/lib/camera/CameraDisplay.svelte
@@ -1,9 +1,57 @@
 <script lang="ts">
-	import { tick } from "svelte"
+	import { tick, onMount } from "svelte"
+
+	let video: HTMLVideoElement
+	let canvas: HTMLCanvasElement
+	let context: CanvasRenderingContext2D
+	
+	onMount(() => {
+		context = canvas.getContext("2d")!;
+	})
+
+	const processor = {
+		timerCallback() {
+			if (video?.paused || video.ended) {
+				return;
+			}
+			this.computeFrame();
+			setTimeout(() => {
+				this.timerCallback();
+			}, 16); // TODO haha this is a really bad idea oh my god
+		},
+
+		doLoad() {
+			width = video.videoWidth;
+			height = video.videoHeight;
+			canvas.width = width;
+			canvas.height = height;
+			tick().then(() => this.timerCallback())
+		},
+
+		computeFrame() {
+			context.drawImage(video, 0, 0, width, height);
+			try {
+				width = video.videoWidth;
+				height = video.videoHeight;
+				const frame = context.getImageData(0, 0, width, height);
+				const l = frame.data.length / 4;
+
+				for (let i = 0; i < l; i++) {
+					const grey = (frame.data[i * 4 + 0] + frame.data[i * 4 + 1] + frame.data[i * 4 + 2]) / 3;
+
+					frame.data[i * 4 + 0] = grey;
+					frame.data[i * 4 + 1] = grey;
+					frame.data[i * 4 + 2] = grey;
+				}
+				context.putImageData(frame, 0, 0);
+			} catch (e) {
+				 console.warn("Could not retrieve frame: ", e)
+			}
+		}
+	};
+
 
 	export let mediaStream: MediaStream;
-	export let previewHeight = '';
-	export let previewWidth = '';
 	export let classes = '';
 
 	export let width = 0;
@@ -31,10 +79,20 @@
 
 <!-- svelte-ignore a11y-media-has-caption -->
 <video
-	class={classes}
+	bind:this={video}
+	style="display: none"
 	use:srcObject={mediaStream}
-	width={previewWidth}
-	height={previewHeight}
+	on:play={() => {
+		width = video.width;
+		height = video.height;
+		processor.doLoad()
+	}}
+	on:resize={() => {
+		width = video.width;
+		height = video.height
+	}}
 	autoplay
 	playsinline
 />
+
+<canvas class={classes} bind:this={canvas}></canvas>
