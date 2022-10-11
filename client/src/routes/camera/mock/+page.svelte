542c00616fef6721aa5b9258bac8a913889ec159
LeoDog896 <LeoDog896@hotmail.com>
2022-10-11 19:41:08 +0000

    mock!

---
+++ client/src/routes/camera/mock/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onDestroy } from 'svelte';
+	import CameraDisplay from '$lib/camera/CameraDisplay.svelte';
+
+	let stream: MediaStream;
+
+	onDestroy(() => {
+		if (!stream) return;
+		for (const track of [...stream.getTracks()]) {
+			track.stop();
+			stream.removeTrack(track);
+		}
+	});
+</script>
+
+{#await getMedia()}
+	<p>Waiting for Camera</p>
+{:then mediaStream}
+	<div class="m-16 bg-gray-100 flex flex-col p-8 rounded-lg shadow-lg">
+		<CameraDisplay {mediaStream} />
+		<div class="h-1/2 grow">
+			<p>Streaming Status</p>
+		</div>
+	</div>
+{/await}
