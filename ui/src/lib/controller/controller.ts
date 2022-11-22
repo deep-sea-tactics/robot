d39f4bb63e78b930e99e9d5b5baeefe1e8a6e7aa
LeoDog896 <LeoDog896@hotmail.com>
2022-11-22 20:57:01 +0000

    Don't listen to controller data

--- ui/src/lib/controller/controller.ts
+++ ui/src/lib/controller/controller.ts
@@ -1,11 +1,6 @@
 import type { ControllerData } from 'landstown-robotics-types';
 import { defaultControllerData } from 'landstown-robotics-types';
 import { writable } from 'svelte/store';
-import { client } from '../socket/socket';
 
 /* A position from 0-100 on two axises, where 0 is the top left and 100 is the bottom right. */
 export const data = writable<ControllerData>(defaultControllerData);
-
-client.on('controllerData', ($data: ControllerData) => {
-	data.set($data);
-});
