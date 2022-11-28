b0d12099802cd33ec4c9a296b3f6a8c30879380b
dylansison <dlanm2u@gmail.com>
2022-11-28 13:41:18 +0000

    tried to fix things and point out issues cuz i don't know any better

--- ui/src/lib/controller/controller.ts
+++ ui/src/lib/controller/controller.ts
@@ -1,4 +1,4 @@
-import type { ControllerData } from 'landstown-robotics-types';
+import type { ControllerData } from 'landstown-robotics-types'; /*gitpod says its not referencing the types, please explain?*/ 
 import { writable } from 'svelte/store';
 
 /* A position from 0-100 on two axises, where 0 is the top left and 100 is the bottom right. */
