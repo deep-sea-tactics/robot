183cabc1b1a41cb1f3491ffe8d0a631a12ecd816
Tristan F <LeoDog896@hotmail.com>
2022-11-27 20:43:14 -0500

    Move render to index

--- robot-mock/src/gl.ts
+++ robot-mock/src/gl.ts
@@ -3,63 +3,7 @@ import gl from 'gl';
 import * as THREE from 'three';
 import type { WebGLRenderer } from 'three';
 
-interface SceneInstance {
-	scene: THREE.Scene;
-	camera: THREE.Camera;
-	step?: () => void;
-}
-
-function createScene(): SceneInstance {
-	let rotation = 0;
-
-	const scene = new THREE.Scene();
-	scene.background = new THREE.Color('papayawhip');
-	scene.fog = new THREE.FogExp2('papayawhip', 0.1);
-	scene.castShadow = true;
-
-	const camera = new THREE.PerspectiveCamera();
-	camera.position.set(1, 1, 3);
-	scene.add(camera);
-
-	const plane = new THREE.Mesh();
-	plane.geometry = new THREE.PlaneGeometry(50, 50);
-	plane.material = new THREE.MeshStandardMaterial({ color: 'burlywood' });
-	plane.receiveShadow = true;
-	plane.rotation.set(-Math.PI / 2, 0, 0);
-	plane.position.set(0, -0.5, 0);
-	scene.add(plane);
-
-	const grid = new THREE.GridHelper(50, 50, 'papayawhip', 'papayawhip');
-	grid.position.set(0, -0.5 + 0.0001, 0);
-	scene.add(grid);
-
-	const box = new THREE.Mesh();
-	box.geometry = new THREE.BoxGeometry();
-	box.scale.set(1, 1, 1);
-	box.material = new THREE.MeshStandardMaterial({ color: 0xff3e00 });
-	box.castShadow = true;
-	scene.add(box);
-
-	const ambientLight = new THREE.AmbientLight('white', 0.6);
-	scene.add(ambientLight);
-
-	const directionalLight = new THREE.DirectionalLight('white', 0.6);
-	directionalLight.position.set(-2, 3, 2);
-	directionalLight.shadow.mapSize.width = 2048;
-	directionalLight.shadow.mapSize.height = 2048;
-	scene.add(directionalLight);
-
-	return {
-		scene,
-		camera,
-		step() {
-			rotation += 0.1;
-			box.rotation.set(0, rotation, 0);
-		},
-	};
-}
-
-function createRenderer({ height, width }: { height: number; width: number }): WebGLRenderer {
+export function createRenderer({ height, width }: { height: number; width: number }): WebGLRenderer {
 	// THREE expects a canvas object to exist, but it doesn't actually have to work.
 	const canvas = {
 		width,
@@ -101,7 +45,7 @@ interface RenderData {
 	pixels: Uint8Array;
 }
 
-function extractPixels(context: WebGLRenderingContext): RenderData {
+export function extractPixels(context: WebGLRenderingContext): RenderData {
 	const width = context.drawingBufferWidth;
 	const height = context.drawingBufferHeight;
 	const frameBufferPixels = new Uint8Array(width * height * 4);
@@ -116,7 +60,7 @@ function extractPixels(context: WebGLRenderingContext): RenderData {
 	return { width, height, pixels };
 }
 
-function output({ width, height, pixels }: RenderData) {
+export function output({ width, height, pixels }: RenderData) {
 	// go through every 2 rows of pixels and output each 2 pixels as a single character
 	const trueWidth = width * 4;
 	for (let row = 0; row < height; row += 2) {
@@ -134,15 +78,3 @@ function output({ width, height, pixels }: RenderData) {
 		process.stdout.write('\n');
 	}
 }
-
-const { scene, camera, step } = createScene();
-
-const renderer = createRenderer({ width: 120, height: 50 });
-renderer.render(scene, camera);
-
-setInterval(() => {
-	step?.();
-	renderer.render(scene, camera);
-	console.clear();
-	output(extractPixels(renderer.getContext()));
-}, 1000 / 10);
--- robot-mock/src/index.ts
+++ robot-mock/src/index.ts
@@ -1,1 +1,71 @@
-import './gl.js';
+import { createRenderer, output, extractPixels } from './gl.js';
+import * as THREE from 'three';
+interface SceneInstance {
+	scene: THREE.Scene;
+	camera: THREE.Camera;
+	step?: () => void;
+}
+
+function createScene(): SceneInstance {
+	let rotation = 0;
+
+	const scene = new THREE.Scene();
+	scene.background = new THREE.Color('papayawhip');
+	scene.fog = new THREE.FogExp2('papayawhip', 0.1);
+	scene.castShadow = true;
+
+	const camera = new THREE.PerspectiveCamera();
+	camera.position.set(1, 1, 3);
+	scene.add(camera);
+
+	const plane = new THREE.Mesh();
+	plane.geometry = new THREE.PlaneGeometry(50, 50);
+	plane.material = new THREE.MeshStandardMaterial({ color: 'burlywood' });
+	plane.receiveShadow = true;
+	plane.rotation.set(-Math.PI / 2, 0, 0);
+	plane.position.set(0, -0.5, 0);
+	scene.add(plane);
+
+	const grid = new THREE.GridHelper(50, 50, 'papayawhip', 'papayawhip');
+	grid.position.set(0, -0.5 + 0.0001, 0);
+	scene.add(grid);
+
+	const box = new THREE.Mesh();
+	box.geometry = new THREE.BoxGeometry();
+	box.scale.set(1, 1, 1);
+	box.position.set(1, 1, 0);
+	box.material = new THREE.MeshStandardMaterial({ color: 0xff3e00 });
+	box.castShadow = true;
+	scene.add(box);
+
+	const ambientLight = new THREE.AmbientLight('white', 0.6);
+	scene.add(ambientLight);
+
+	const directionalLight = new THREE.DirectionalLight('white', 0.6);
+	directionalLight.position.set(-2, 3, 2);
+	directionalLight.shadow.mapSize.width = 2048;
+	directionalLight.shadow.mapSize.height = 2048;
+	scene.add(directionalLight);
+
+	return {
+		scene,
+		camera,
+		step() {
+			rotation += 0.1;
+			box.rotation.set(0, rotation, 0);
+		},
+	};
+}
+
+const { scene, camera, step } = createScene();
+
+const renderer = createRenderer({ width: 120, height: 50 });
+renderer.render(scene, camera);
+
+setInterval(() => {
+	step?.();
+	renderer.render(scene, camera);
+	const pixels = extractPixels(renderer.getContext());
+	console.clear();
+	output(pixels);
+}, 1000 / 10);
