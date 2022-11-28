1e59b5deac2a4bcd75a08bec9e868765da451a64
Tristan F <LeoDog896@hotmail.com>
2022-11-27 20:18:31 -0500

    a little cube

--- robot-mock/src/gl.ts
+++ robot-mock/src/gl.ts
@@ -3,20 +3,60 @@ import gl from 'gl';
 import * as THREE from 'three';
 import type { WebGLRenderer } from 'three';
 
-function createScene() {
+interface SceneInstance {
+	scene: THREE.Scene;
+	camera: THREE.Camera;
+	step?: () => void;
+}
+
+function createScene(): SceneInstance {
+	let rotation = 0;
+
 	const scene = new THREE.Scene();
+	scene.background = new THREE.Color('papayawhip');
+	scene.fog = new THREE.FogExp2('papayawhip', 0.1);
+	scene.castShadow = true;
 
-	const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+	const camera = new THREE.PerspectiveCamera();
+	camera.position.set(1, 1, 3);
+	scene.add(camera);
 
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	const cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
-	cube.rotation.set(0, Math.PI / 4, 0);
+	const plane = new THREE.Mesh();
+	plane.geometry = new THREE.PlaneGeometry(50, 50);
+	plane.material = new THREE.MeshStandardMaterial({ color: 'burlywood' });
+	plane.receiveShadow = true;
+	plane.rotation.set(-Math.PI / 2, 0, 0);
+	plane.position.set(0, -0.5, 0);
+	scene.add(plane);
 
-	camera.position.z = 5;
+	const grid = new THREE.GridHelper(50, 50, 'papayawhip', 'papayawhip');
+	grid.position.set(0, -0.5 + 0.0001, 0);
+	scene.add(grid);
 
-	return { scene, camera };
+	const box = new THREE.Mesh();
+	box.geometry = new THREE.BoxGeometry();
+	box.scale.set(1, 1, 1);
+	box.material = new THREE.MeshStandardMaterial({ color: 0xff3e00 });
+	box.castShadow = true;
+	scene.add(box);
+
+	const ambientLight = new THREE.AmbientLight('white', 0.6);
+	scene.add(ambientLight);
+
+	const directionalLight = new THREE.DirectionalLight('white', 0.6);
+	directionalLight.position.set(-2, 3, 2);
+	directionalLight.shadow.mapSize.width = 2048;
+	directionalLight.shadow.mapSize.height = 2048;
+	scene.add(directionalLight);
+
+	return {
+		scene,
+		camera,
+		step() {
+			rotation += 0.1;
+			box.rotation.set(0, rotation, 0);
+		},
+	};
 }
 
 function createRenderer({ height, width }: { height: number; width: number }): WebGLRenderer {
@@ -77,20 +117,32 @@ function extractPixels(context: WebGLRenderingContext): RenderData {
 }
 
 function output({ width, height, pixels }: RenderData) {
-	process.stdout.write(['P3\n# gl.ppm\n', width, ' ', height, '\n255\n'].join(''));
-
-	for (let i = 0; i < pixels.length; i += 4) {
-		process.stdout.write(chalk.rgb(pixels[i], pixels[i + 1], pixels[i + 2])('█'));
-
-		if ((i / 4 + 1) % width === 0) {
-			process.stdout.write('\n');
+	// go through every 2 rows of pixels and output each 2 pixels as a single character
+	const trueWidth = width * 4;
+	for (let row = 0; row < height; row += 2) {
+		for (let i = 0; i < trueWidth; i += 4) {
+			process.stdout.write(
+				chalk
+					.rgb(pixels[i + (row * trueWidth)], pixels[(i + 1) + (row * trueWidth)], pixels[(i + 2) + (row * trueWidth)])
+					.bgRgb(
+						pixels[i + ((row + 1) * trueWidth)],
+						pixels[(i + 1) + ((row + 1) * trueWidth)],
+						pixels[(i + 2) + ((row + 1) * trueWidth)],
+					)('▀'),
+			);
 		}
+		process.stdout.write('\n');
 	}
 }
 
-const { scene, camera } = createScene();
+const { scene, camera, step } = createScene();
 
-const renderer = createRenderer({ width: 200, height: 200 });
+const renderer = createRenderer({ width: 120, height: 50 });
 renderer.render(scene, camera);
 
-output(extractPixels(renderer.getContext()));
+setInterval(() => {
+	step?.();
+	renderer.render(scene, camera);
+	console.clear()
+	output(extractPixels(renderer.getContext()));
+}, 1000 / 10);
