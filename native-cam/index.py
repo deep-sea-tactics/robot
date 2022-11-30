929e00bd450b9b51e150ea719e307bb9142dbf46
dylansison <51481478+dylansison@users.noreply.github.com>
2022-11-30 04:56:02 +0000

    preliminary attempts at extending something i don't really know: pt1

--- native-cam/index.py
+++ native-cam/index.py
@@ -1,8 +1,36 @@
 import socketio
 import aiortc
 sio = socketio.Client()
+from aiortc import RTCPeerConnection, RTCSessionDescription
 
 sio.connect('http://localhost:3000')
 
-sio.emit('broadcaster', {'foo': 'bar'})
+@sio.on('connect')
+def connectedToServer():
+	sio.emit('broadcaster', {'foo': 'bar'})
+
+rtcpc = RTCPeerConnection()
+
+@sio.event
+def candidate(id,message):
+	rtcpc.addIceCandidate(message)
+
+@sio.event
+def offer(id,message):
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    
+
 #need to use aiortc for connection
+
+#	typings reference cuz same words used by socketio i think???
+#
+#	export interface ClientToServerMap {
+#		controllerData: (data: ControllerData) => void;
+#		/** A broadcaster is ready to broadcast */
+#		broadcaster: () => void;
+#		watcher: () => void;
+#		offer: (id: string, message: RTCSessionDescription) => void;
+#		answer: (id: string, message: RTCSessionDescription) => void;
+#		candidate: (id: string, message: RTCIceCandidateInit) => void;
+#	}
