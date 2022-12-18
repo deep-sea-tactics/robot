e36799e6b1c56c5f3508c63f9cc24d4905915302
Tristan F <LeoDog896@hotmail.com>
2022-12-18 19:13:04 +0000

    attempt to gather and send out ice candidates

--- native-camera/__init__.py
+++ native-camera/__init__.py
@@ -1,19 +1,19 @@
 import socketio
 import asyncio
 from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer, MediaRelay
+from aiortc.contrib.media import MediaPlayer
+from aiortc.sdp import candidate_from_sdp
 from typing import Dict
 
 # Set up media relay
 socketClient = socketio.AsyncClient()
 player = MediaPlayer('../videos/robert.mp4')
-relay = MediaRelay()
 
 # Subscribe to the video (creates the media tracts)
-video = relay.subscribe(player.video)
+video = player.video
 
 # pairs of SocketIO ids to their respective PeerConnections
-peerConnections: Dict[str, RTCPeerConnection] = dict()
+peerConnections: Dict[str, RTCPeerConnection] = {}
 
 
 @socketClient.event
@@ -30,6 +30,20 @@ async def connect_error(data):
 @socketClient.event
 async def disconnect():
     print("Self disconnected.")
+
+
+@socketClient.event
+async def candidate(id, candidate):
+    print(f"Received ice candidate: {candidate}")
+    if len(candidate["candidate"]) == 0:
+        return
+
+    iceCandidate = candidate_from_sdp(candidate["candidate"].split(":", 1)[1])
+    iceCandidate.sdpMid = candidate["sdpMid"]
+    iceCandidate.sdpMLineIndex = candidate["sdpMLineIndex"]
+
+    peerConnection = peerConnections[id]
+    await peerConnection.addIceCandidate(iceCandidate)
 
 
 @socketClient.event
@@ -53,6 +67,21 @@ async def watcher(id):
     async def on_connectionstatechange():
         print("Connection state is %s" % peerConnection.connectionState)
         if peerConnection.connectionState == "failed":
+            if not peerConnections.has_key(peerConnection):
+                return
+            await peerConnection.close()
+            del peerConnections[peerConnection]
+        elif peerConnection.connectionState == "connected":
+            print(peerConnection.sctp)
+
+    @peerConnection.on("iceconnectionstatechange")
+    async def on_iceconnectionstatechange():
+        print("ICE Connection state is %s" % peerConnection.iceConnectionState)
+        if peerConnection.iceConnectionState == "failed":
+            if not peerConnections.has_key(peerConnection):
+                print("Peer connection not found")
+                return
+            print("Closing peer connection due to ICE failure")
             await peerConnection.close()
             del peerConnections[peerConnection]
 
@@ -68,6 +97,27 @@ async def watcher(id):
         "sdp": peerConnection.localDescription.sdp,
         "type": peerConnection.localDescription.type
     }))
+
+    @peerConnection.on('icegatheringstatechange')
+    async def on_icegatheringstatechange():
+        print(f'iceGatheringState changed: {peerConnection.iceGatheringState}')
+        if peerConnection.iceGatheringState == 'complete':
+            if peerConnection.sctp is None:
+                print("SCTP is not ready yet")
+                return
+
+            # candidates are ready
+            transport = peerConnection.sctp.transport.transport
+            iceCandidates = transport.iceGatherer.getLocalCandidates()
+            for iceCandidate in iceCandidates:
+                print(f"Sending ice candidate: {iceCandidate}")
+                candidate = {
+                    "candidate": iceCandidate.candidate,
+                    "sdpMid": iceCandidate.sdpMid,
+                    "sdpMLineIndex": iceCandidate.sdpMLineIndex
+                }
+                print(f"Sending ice candidate: {candidate}")
+                await socketClient.emit("candidate", (id, candidate))
 
 
 async def main():
