8dfe3cc575f8e52d8e4055e017b00c229a962672
Tristan F <LeoDog896@hotmail.com>
2022-12-18 16:03:35 -0500

    Set up protolint, begin proto

---
+++ scripts/protolint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+	protolint --fix ../protocol/main.proto
+}
+
+main "$@"
