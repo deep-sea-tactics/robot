e3c4733b720e3640a37e7d55b3613b24bd6ba81e
Tristan F <LeoDog896@hotmail.com>
2022-12-18 20:51:03 -0500

    Revert "Set up protolint, begin proto"
    This reverts commit 8dfe3cc575f8e52d8e4055e017b00c229a962672.

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,8 +10,7 @@ fi
 cd "$(dirname "$0")"
 
 main() {
-	go install github.com/yoheimuta/protolint/cmd/protolint@latest # install protolint
-	npm i -g nodemon # install nodemon
+	sudo npm i -g nodemon # install nodemon
 	yarn # install yarn dependencies
 	(cd ../native-camera; pip install -r requirements.txt) # install poetry dependencies in native-camera
 	(cd ../landstown-robotics-types; yarn build) # build typings
