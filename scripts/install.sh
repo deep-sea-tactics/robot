635b8d92bdfef6cfe86d598f960fcafff837fa9c
Tristan F <LeoDog896@hotmail.com>
2023-01-18 12:41:51 +0000

    chore(deps): update deps

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,9 +10,7 @@ fi
 cd "$(dirname "$0")"
 
 main() {
-	sudo npm i -g nodemon # install nodemon
 	yarn # install yarn dependencies
-	(cd ../native-camera; pip install -r requirements.txt) # install poetry dependencies in native-camera
 	(cd ../landstown-robotics-types; yarn build) # build typings
 	./install-streamer.sh # installs webrtc-streamer
 }
