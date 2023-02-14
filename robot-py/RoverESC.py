93ddfcce953c83b2e568c065e399a3c49f629448
nl937 <nathanlong937@gmail.com>
2023-02-14 20:29:03 +0000

    Redefine ESC pins, add methods for additional ESCs

--- robot-py/RoverESC.py
+++ robot-py/RoverESC.py
@@ -8,10 +8,12 @@ import pigpio #importing GPIO library
 # Constant values
 # -----------------------------------------
 # Gpio pins
-MOTOR_1=5  # Right motor
-MOTOR_2=27  # Left motor
-MOTOR_3=20  # Right-Up motor
-MOTOR_4=24   # Left-Up motor
+MOTOR_1=16  # Right motor
+MOTOR_2=19  # Left motor
+MOTOR_3=13  # Right-Up motor
+MOTOR_4=12   # Left-Up motor
+MOTOR_5=6
+MOTOR_6=5
 
 # PWM values for the gio pins
 MAX_VALUE = 1900 # ESC's max value
@@ -27,15 +29,19 @@ pi.set_servo_pulsewidth(MOTOR_1, 0)
 pi.set_servo_pulsewidth(MOTOR_2, 0) 
 pi.set_servo_pulsewidth(MOTOR_3, 0) 
 pi.set_servo_pulsewidth(MOTOR_4, 0) 
+pi.set_servo_pulsewidth(MOTOR_5, 0) 
+pi.set_servo_pulsewidth(MOTOR_6, 0) 
 time.sleep(1)
 # 1500 sets the motor speeds off
 pi.set_servo_pulsewidth(MOTOR_1, ZERO_VALUE) 
 pi.set_servo_pulsewidth(MOTOR_2, ZERO_VALUE) 
 pi.set_servo_pulsewidth(MOTOR_3, ZERO_VALUE) 
 pi.set_servo_pulsewidth(MOTOR_4, ZERO_VALUE) 
+pi.set_servo_pulsewidth(MOTOR_5, ZERO_VALUE) 
+pi.set_servo_pulsewidth(MOTOR_6, ZERO_VALUE) 
 
 print ('ESCs ready to control')
-print ('MOTOR_1: gpio-'+str(MOTOR_1)+', MOTOR_2: gpio-'+str(MOTOR_2)+', MOTOR_3: gpio-'+str(MOTOR_3)+', MOTOR_4: gpio-'+str(MOTOR_4) )
+print ('MOTOR_1: gpio-'+str(MOTOR_1)+', MOTOR_2: gpio-'+str(MOTOR_2)+', MOTOR_3: gpio-'+str(MOTOR_3)+', MOTOR_4: gpio-'+str(MOTOR_4) +', MOTOR_3: gpio-'+str(MOTOR_5)+', MOTOR_3: gpio-'+str(MOTOR_6))
 
 # -----------------------------------------
 # Method Definitions
@@ -57,9 +63,19 @@ def motor4_go(inp): #You will use this function to program your ESC if required
 	# print "Motor-4 speed to " + str(inp)
 	pi.set_servo_pulsewidth(MOTOR_4,inp)
 
+def motor5_go(inp): #You will use this function to program your ESC if required
+	# print "Motor-5 speed to " + str(inp)
+	pi.set_servo_pulsewidth(MOTOR_5,inp)
+
+def motor6_go(inp): #You will use this function to program your ESC if required
+	# print "Motor-6 speed to " + str(inp)
+	pi.set_servo_pulsewidth(MOTOR_6,inp)
+
 def stop_all(): 
 	pi.set_servo_pulsewidth(MOTOR_1, 0)
 	pi.set_servo_pulsewidth(MOTOR_2, 0)
 	pi.set_servo_pulsewidth(MOTOR_3, 0)
 	pi.set_servo_pulsewidth(MOTOR_4, 0)
+	pi.set_servo_pulsewidth(MOTOR_5, 0)
+	pi.set_servo_pulsewidth(MOTOR_6, 0)
 	pi.stop()
