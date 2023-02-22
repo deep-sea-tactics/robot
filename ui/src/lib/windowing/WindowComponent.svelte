d06cd89b4c3cecdc4e26a1110eebf9ec7c882a10
William Faircloth <willuhmjs@gmail.com>
2023-02-22 23:00:54 +0000

    add comment to address issue #9

--- ui/src/lib/windowing/WindowComponent.svelte
+++ ui/src/lib/windowing/WindowComponent.svelte
@@ -148,7 +148,10 @@
 			{windowName}
 			<div class="dockable-icon">
 				<div
-					on:click={() => (localZIndex = 0)}
+					on:click={() => { 
+						/* make the height of the window here 0 here */
+						localZIndex = 0;
+					 }}
 					on:keydown={event => {
 						if (event.key == 'Enter') localZIndex = 0;
 					}}
