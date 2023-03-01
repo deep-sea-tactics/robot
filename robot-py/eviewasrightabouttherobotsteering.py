4a51033002aa1846c16ba293370a67da7db2f822
dylansison <51481478+dylansison@users.noreply.github.com>
2023-03-01 02:05:19 +0000

    evie did a thing lol

--- robot-py/eviewasrightabouttherobotsteering.py
+++ robot-py/eviewasrightabouttherobotsteering.py
@@ -1,1 +1,29 @@
-math 
+var inputx = something;
+var inputy = something else;
+var inputz = another something;
+
+var inputa = rotational input of awesomeness
+var input b = spinny
+var input c = hypothetical?? input
+
+
+//MATH NOT SHOWN???
+
+var totalbitofpowerused = that math that wasnt shown
+var totalbitofpowerusedprecent = totalbitofpowerused/20
+// that math that wasnt shown should be less than or equal to 20
+
+// mathe war nicht schreiben
+
+var precentforward = mathe war nicht schreiben 1
+var precentrotate = mathe war nicht schreiben 2
+
+var xrotate = //math that finds how much to rotate on x direction (%)
+var yrotate = see above
+var zrotate = see above
+
+var xforward = //math that finds how much moving in x direction (%)
+var yforward = above
+var zforward = avode
+
+var x = ((precentrotate * xrotate) + (xforward * precentforward)) // precent of x motor moving
