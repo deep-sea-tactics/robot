b9f4fef5782ccd809c85d548f30e4dd99ec82f7d
dylansison <51481478+dylansison@users.noreply.github.com>
2023-03-30 13:44:52 -0400

    amps to pwm conversion in progress

--- robot-py/fn-dynamicpoweralloc.py
+++ robot-py/fn-dynamicpoweralloc.py
@@ -1,17 +1,37 @@
 import numpy as np 
-powerRequestArray = [1,1,0.5,0.5,0,0] #debug
-currentLimit=20  # max current (fuse) minus current for electronics (cameras,pi,arm)
-def powerRequests(powerRequestArray,debug=False):
-	rqArr = list(np.array(powerRequestArray)*100) # simplifies equation entry: make sure that incoming values are decimal out of 1
-	x = np.array(rqArr) # simplifies equuation entry
-	requestAdjusted = list((3.44*10**-3)+(2.64*10**-2)*x+(1.8*10**-3)*x**2+(3.31*10**-5)*x**3+(-1.15*10**-6)*x**4+(1.2*10**-8)*x**5+(-4.23*10**-11)*x**6) # polynomial in terms of x: THIS CURVE CALCULATED FOR BLUEROBOTICS T200 out of 100
-	fullPowerAdjusted = ((3.44*10**-3)+(2.64*10**-2)*100+(1.8*10**-3)*100**2+(3.31*10**-5)*100**3+(-1.15*10**-6)*100**4+(1.2*10**-8)*100**5+(-4.23*10**-11)*100**6) # when x in requestAdjusted = 100 
-	powerAmpsOut = np.around((((np.array(requestAdjusted))**2)*currentLimit)/(sum(requestAdjusted)*(fullPowerAdjusted)),6)
-	return(powerAmpsOut)
-	if debug == True:	
-		print(requestAdjusted)
-		print(fullPowerAdjusted)
-		print(powerAmpsOut)
-		print(sum(powerAmpsOut))
 
-powerRequests(powerRequestArray,True) #temp,debug
+powerRequestArray = [1,1,0.5,0.5,0,0] #debug
+currentLimit=20  # marqArr current (fuse) minus current for electronics (cameras,pi,arm)
+
+class dynamicpoweralloc(orders,motorPins)
+	def calcLimit(x):
+		# equation derived from https://docs.google.com/spreadsheets/d/1rIfVPKC-plG1GTZuCKPb0hEpbHQJ4Zlnyg2ltgLpQZw/edit?usp=sharing
+		return (3.44*10**-3)+(2.64*10**-2)*x+(1.8*10**-3)*x**2+(3.31*10**-5)*x**3+(-1.15*10**-6)*x**4+(1.2*10**-8)*x**5+(-4.23*10**-11)*x**6
+
+	def powerRequests(powerRequestArray,debug=False):
+		rqArr = np.array(powerRequestArray)*100 # simplifies equation entry: make sure that incoming values are decimal out of 1
+		requestAdjusted = calcLimit(rqArr) # polynomial in terms of rqArr: THIS CURVE CALCULATED FOR BLUEROBOTICS T200 out of 100
+		fullPowerAdjusted = calcLimit(100) # when rqArr in requestAdjusted = 100 
+		powerAmpsOut = np.around((((requestAdjusted)**2)*currentLimit)/(sum(requestAdjusted)*(fullPowerAdjusted)),6)
+		return(powerAmpsOut)
+		if debug == True:	
+			print(requestAdjusted)
+			print(fullPowerAdjusted)
+			print(powerAmpsOut)
+			print(sum(powerAmpsOut))
+
+	def ampsToPWMEQ(amps,orders):
+		for (amps,orders) in zip(amps,orders)
+			match orders:
+				case orders < 0:
+					return (1449)+(-100)*amps+(29.1)*amps**2+(-5.27)*amps**3+(0.504)*amps**4+(-0.0238)*amps**5+((4.38*10**-4))*amps**6
+				case orders == 0:
+					return 0
+				case orders > 0:
+					return (1552)+(96.9)*amps +(-27.8)*amps**2+(5.02)*amps**3+(-0.481)*amps**4+(0.0228)*amps**5+(-4.2*10**-4)amps**6
+
+
+
+		
+dynamicpoweralloc.ampsToPWMEQ([15,0,15],[1,0,-1])	
+dynamicpoweralloc.powerRequests(powerRequestArray,True) #temp,debug
