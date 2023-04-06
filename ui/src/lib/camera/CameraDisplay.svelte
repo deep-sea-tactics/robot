476833dc5276f8ce4db22a14ab577a85b9fe8daf
Tristan F <LeoDog896@hotmail.com>
2023-04-06 19:50:45 +0000

    refactor: remove camera processor

--- ui/src/lib/camera/CameraDisplay.svelte
+++ ui/src/lib/camera/CameraDisplay.svelte
@@ -1,55 +1,9 @@
 <script lang="ts">
-	import { tick, onMount } from 'svelte';
 	import consola from 'consola';
 
 	let video: HTMLVideoElement;
-	let canvas: HTMLCanvasElement;
-	let context: CanvasRenderingContext2D;
-
-	onMount(() => {
-		const localContext = canvas.getContext('2d');
-		if (!localContext) throw Error('No 2d context found. Weird?');
-		context = localContext;
-	});
-
-	const processor = {
-		timerCallback() {
-			if (video?.paused || video?.ended) {
-				return;
-			}
-			this.computeFrame();
-			setTimeout(() => {
-				this.timerCallback();
-			}, 16); // NOTE i can only imagine the consequences of this
-		},
-
-		doLoad() {
-			width = video.videoWidth;
-			height = video.videoHeight;
-			canvas.width = width;
-			canvas.height = height;
-			tick().then(() => this.timerCallback());
-		},
-
-		computeFrame() {
-			context.drawImage(video, 0, 0, width, height);
-			try {
-				width = video.videoWidth;
-				height = video.videoHeight;
-				const frame = context.getImageData(0, 0, width, height);
-
-				context.putImageData(frame, 0, 0);
-			} catch (e) {
-				console.warn('Could not retrieve frame: ', e);
-			}
-		}
-	};
 
 	export let mediaStream: MediaStream;
-	let classes = '';
-	export let style = '';
-
-	export { classes as class };
 
 	let width = 0;
 	let height = 0;
@@ -77,12 +31,10 @@
 		bind:this={video}
 		bind:clientWidth={width}
 		bind:clientHeight={height}
-		style="display: none"
 		use:srcObject={mediaStream}
 		on:play={() => {
 			width = video.width;
 			height = video.height;
-			processor.doLoad();
 		}}
 		on:resize={() => {
 			width = video.width;
@@ -94,17 +46,3 @@
 		<track kind="captions" />
 	</video>
 {/if}
-
-<canvas
-	{style}
-	{width}
-	{height}
-	class={classes}
-	bind:this={canvas}
-/>
-
-<style>
-	canvas {
-		width: 100%;
-	}
-</style>
