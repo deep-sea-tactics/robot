f238f3ba8d448dba3d4d3baf5c3e1b6646cc0c6c
dylansison <51481478+dylansison@users.noreply.github.com>
2023-04-21 14:52:43 -0400

    Revert "chore: dev -> preview"
    This reverts commit 816d409415cb223ee4191f8e9007c68c706278e4.

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,7 +13,7 @@ main() {
 	# kill all subshells and processes on exit
 	trap "kill 0" SIGINT
 	# start commands in subshells so all their spawn DIE when we exit
-	( cd ../ui; yarn preview ) &
+	( cd ../ui; yarn dev ) &
 	( cd ../relay; yarn dev ) &
 	wait
 }
