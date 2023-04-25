fd9d88aa3b3fe3e52623a96cbc38393d7a096dbf
William Faircloth <willuhmjs@gmail.com>
2023-04-25 19:10:04 +0000

    feat: new robot.sh

--- scripts/robot.sh
+++ scripts/robot.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-python3 robot-py/robot.py
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+	# kill all subshells and processes on exit
+	trap "kill 0" SIGINT
+	# start commands in subshells so all their spawn DIE when we exit
+	( ./stream-run.sh ) &
+	( cd ../robot-py; python controller.py ) &
+	wait
+}
+
+main "$@"
