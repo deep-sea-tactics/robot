d601b2f1b45e0d911260f4df27ab25201433496b
LeoDog896 <leodog896@hotmail.com>
2023-05-01 13:46:17 -0400

    feat: easier run script

---
+++ robot.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+	# kill all subshells and processes on exit
+	trap "kill 0" SIGINT
+	# start commands in subshells so all their spawn DIE when we exit
+	( scripts/stream-run.sh ) &
+	( cd robot-py; python controller.py ) &
+	wait
+}
+
+main "$@"
