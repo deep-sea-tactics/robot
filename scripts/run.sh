d80ad2b2ba1801c34971556c7c50bcb376839f5a
LeoDog896 <leodog896@hotmail.com>
2023-05-02 16:08:30 -0400

    feat: proper robot run

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,8 +13,8 @@ main() {
 	# kill all subshells and processes on exit
 	trap "kill 0" SIGINT
 	# start commands in subshells so all their spawn DIE when we exit
-	( cd ../ui; yarn preview ) &
-	( cd ../relay; yarn start ) &
+	( sh stream-run.sh  ) &
+	( cd ../robot-py; python controller.py ) &
 	wait
 }
 
