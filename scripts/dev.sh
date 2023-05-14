4495417465ca294083eebfa18451de05fa0a1827
Tristan F <LeoDog896@hotmail.com>
2023-05-13 20:25:39 -0400

    refactor: fully drop relay, connect to UI directly

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,14 +10,8 @@ fi
 cd "$(dirname "$(readlink -f "$0")")"
 
 main() {
-	(cd ../landstown-robotics-types; yarn build)
-
-	# kill all subshells and processes on exit
-	trap "kill 0" SIGINT
-	# start commands in subshells so all their spawn DIE when we exit
-	( cd ../ui; yarn dev ) &
-	( cd ../relay; yarn dev ) &
-	wait
+	cd ../ui
+	yarn dev
 }
 
 main "$@"
--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-fuser -k 3000/tcp # kills socket.io server
 fuser -k 4000/tcp # kills ui
