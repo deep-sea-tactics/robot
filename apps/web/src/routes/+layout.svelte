ea6954671f4f1dc2ab402d1d14b61cdcfe5b0a57
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-09-12 18:29:33 +0000

    Made not one but 2 boxes

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -5,5 +5,12 @@
         margin: 0px;
         padding: 0px;
         background: #403D39;
+
+
+        --bg: #403D39;
+        --bgDark: #252422;
+        --accent: #2C3289;
+        --light: #CCC5B9;
+        --text: #FFFCF2;
     }
 </style>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -3,7 +3,25 @@
 </script>
 
 <div class="wrap">
-    <div>
-        
+    <div class="grid">
+
+    </div>
+    <div class="bottomBar">
     </div>
 </div>
+
+
+
+<style>
+    .grid {
+        min-height: 10vh;
+        height: 80vh;
+    }
+    .bottomBar {
+        height: 20vh;
+        min-height: 10vh;
+        background: var(--bgDark);
+        display: flex;
+        flex-direction: row;
+    }
+</style>
