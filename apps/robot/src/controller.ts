c0e2652d8604362d3c0e7686c889fbc885d3e45d
Tristan F <LeoDog896@hotmail.com>
2023-10-03 10:08:26 -0400

    style: fmt

--- apps/robot/src/controller.ts
+++ apps/robot/src/controller.ts
@@ -1,5 +1,4 @@
-import { z } from "zod";
-
+import { z } from 'zod';
 
 /**
  * General X and Y position interface.
@@ -10,39 +9,38 @@ import { z } from "zod";
  * 0x means left, 100x means right
  */
 export const PositionSchema = z.object({
-	x: z.number().min(0).max(100),
-	y: z.number().min(0).max(100),
-})
+  x: z.number().min(0).max(100),
+  y: z.number().min(0).max(100)
+});
 
 export type Position = z.infer<typeof PositionSchema>;
-
 
 /**
  * All data sent from the controller
  */
 export const ControllerDataSchema = z.object({
-	position: PositionSchema,
-	yaw: z.number(),
-	view: PositionSchema,
-	throttle: z.number(),
-	buttons: z.object({
-		trigger: z.boolean(),
-		side_grip: z.boolean(),
-		side_panel: z.object({
-			bottom_left: z.boolean(),
-			top_left: z.boolean(),
-			bottom_middle: z.boolean(),
-			top_middle: z.boolean(),
-			bottom_right: z.boolean(),
-			top_right: z.boolean(),
-		}),
-		controller_buttons: z.object({
-			top_left: z.boolean(),
-			top_right: z.boolean(),
-			bottom_left: z.boolean(),
-			bottom_right: z.boolean(),
-		}),
-	}),
-})
+  position: PositionSchema,
+  yaw: z.number(),
+  view: PositionSchema,
+  throttle: z.number(),
+  buttons: z.object({
+    trigger: z.boolean(),
+    side_grip: z.boolean(),
+    side_panel: z.object({
+      bottom_left: z.boolean(),
+      top_left: z.boolean(),
+      bottom_middle: z.boolean(),
+      top_middle: z.boolean(),
+      bottom_right: z.boolean(),
+      top_right: z.boolean()
+    }),
+    controller_buttons: z.object({
+      top_left: z.boolean(),
+      top_right: z.boolean(),
+      bottom_left: z.boolean(),
+      bottom_right: z.boolean()
+    })
+  })
+});
 
 export type ControllerData = z.infer<typeof ControllerDataSchema>;
--- apps/robot/src/index.ts
+++ apps/robot/src/index.ts
@@ -1,13 +1,13 @@
-import { initTRPC } from "@trpc/server";
-import { ControllerDataSchema } from "./controller";
+import { initTRPC } from '@trpc/server';
+import { ControllerDataSchema } from './controller';
 
 const t = initTRPC.create();
 
 const router = t.router({
-    controller_data: t.procedure.input(ControllerDataSchema).query(({ input }) => {
-        console.log(input);
-        return input;
-    }) 
+  controller_data: t.procedure.input(ControllerDataSchema).query(({ input }) => {
+    console.log(input);
+    return input;
+  })
 });
 
 export type RobotRouter = typeof router;
