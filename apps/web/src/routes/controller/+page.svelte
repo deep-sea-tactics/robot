b8c66755b9cdf2dd6e301a7ef294e08d1aa8d6c5
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-08 18:26:05 -0500

    refactor: move controller into separate component

--- apps/web/src/routes/controller/+page.svelte
+++ apps/web/src/routes/controller/+page.svelte
@@ -3,52 +3,12 @@
 </svelte:head>
 
 <script lang="ts">
-  import { onMount } from "svelte";
-  import type { ControllerData } from "robot/dist/controller";
-  let gamepad: Gamepad | null;
+    import Controller from "$lib/components/Controller.svelte";
+    import type { ControllerData } from "robot/dist/controller";
 
-  let output: ControllerData;
-
-  onMount(() => {
-    window.addEventListener("gamepadconnected", (e) => {
-      gamepad = navigator.getGamepads()[e.gamepad.index];
-      requestAnimationFrame(getInput)
-    })
-  })
-
-  function getInput() {
-    gamepad = navigator.getGamepads()[0];
-    if (!gamepad) return
-    output = {
-      buttons: {
-        trigger: gamepad.buttons[0].pressed,
-        side_grip: gamepad.buttons[1].pressed,
-        joystick: {
-          lowerLeft: gamepad.buttons[2].pressed,
-          lowerRight: gamepad.buttons[3].pressed,
-          upperLeft: gamepad.buttons[4].pressed,
-          upperRight: gamepad.buttons[5].pressed,
-        },
-        grid: {
-          topLeft: gamepad.buttons[6].pressed,
-          topRight: gamepad.buttons[7].pressed,
-          middleLeft: gamepad.buttons[8].pressed,
-          middleRight: gamepad.buttons[9].pressed,
-          bottomLeft: gamepad.buttons[10].pressed,
-          bottomRight: gamepad.buttons[11].pressed,
-        }
-      },
-      position: {
-        x: gamepad.axes[0],
-        y: gamepad.axes[1],
-      },
-      yaw: gamepad.axes[5],
-      throttle: gamepad.axes[6],  
-      view: gamepad.axes[9]
-    }
-
-    requestAnimationFrame(getInput)
-  }
+    let output: ControllerData;
 </script>
+
+<Controller bind:output />
 
 <pre>{JSON.stringify(output,null,2)}</pre>
