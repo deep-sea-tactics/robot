3e8f6c30d8d525c5c422838f0b17c78881596a37
LeoDog896 <LeoDog896@hotmail.com>
2023-12-14 17:03:58 +0000

    feat: move trpc server -> wss

--- apps/robot/src/index.ts
+++ apps/robot/src/index.ts
@@ -1,9 +1,20 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { applyWSSHandler } from '@trpc/server/adapters/ws';
+import ws from 'ws';
 import { router } from './server.js';
 
-const server = createHTTPServer({
+const wss = new ws.Server({
+  port: 9000
+})
+
+const server = applyWSSHandler({
+  wss,
   router
 });
 
 console.log('Listening on http://localhost:9000');
-server.listen(9000);
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM');
+  server.broadcastReconnectNotification();
+  wss.close();
+});
