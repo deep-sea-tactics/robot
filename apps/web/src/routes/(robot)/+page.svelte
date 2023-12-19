d0fb577d7c8013be368c2fbabee2a9a9257ef43d
William Faircloth <willuhmjs@gmail.com>
2023-12-19 19:29:22 +0000

    feat: epic controller flexbox (literally i3)

--- apps/web/src/routes/(robot)/+page.svelte
+++ apps/web/src/routes/(robot)/+page.svelte
@@ -3,22 +3,42 @@
 </script>
 
 <div class="wrap">
-	<div class="grid" />
-	<div class="bottomBar">
+	<div class="grid flexContainer">
+		<div class="item">
+			<p>test</p>
+		</div>
+		<div class="item">
+			<p>test</p>
+		</div>
+		<div class="item">
+			<p>test</p>
+		</div>
+	</div>
+	<div class="bottomBar flexContainer">
 		<ControllerHandler />
 	</div>
 </div>
 
 <style>
+	.flexContainer > * {
+		border: 1px solid var(--light);
+		flex: 1;
+	}
+
+	.wrap {
+		color: var(--text);
+	}
+
 	.grid {
 		min-height: 10vh;
 		height: 80vh;
+		display: flex;
+		flex-direction: row;
 	}
 	.bottomBar {
 		height: 20vh;
 		min-height: 10vh;
 		background: var(--bgDark);
-		display: flex;
 		flex-direction: row;
 	}
 </style>
