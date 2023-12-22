3ecbaa34e79544bf6e2b48b047ae22b4580c0211
William Faircloth <willuhmjs@gmail.com>
2023-12-22 02:02:38 -0800

    refactor: move handler to +page.svelte

--- apps/web/src/routes/(robot)/+page.svelte
+++ apps/web/src/routes/(robot)/+page.svelte
@@ -1,6 +1,54 @@
 <script lang="ts">
-	import StickHandler from '$lib/components/StickHandler.svelte';
+	import { Canvas, Layer, type Render } from 'svelte-canvas';
+	import Controller from '$lib/components/handlers/Controller.svelte';
+	import Keyboard from '$lib/components/handlers/Keyboard.svelte';
+	import type { ControllerData  } from 'robot/dist/controller';
+	import { client } from '$lib/connections/robot';
+
+	let gamepad: Gamepad;
+	let gamepadOutput: ControllerData;
+	let keyboardOutput: ControllerData;
+	$: output = gamepadOutput ?? keyboardOutput;
+	$: if (output) client?.controllerData.mutate(output);
+
+
+	const renderStick: (stick: "leftStick" | "rightStick") => Render = (stick) => ({ context, width, height }) => {
+		if (!output) return;
+		const x = output.axes[stick].x;
+		const y = output.axes[stick].y;
+
+		context.font = `1rem sans-serif`;
+		context.textAlign = 'center';
+		context.textBaseline = 'middle';
+		context.fillStyle = 'tomato';
+		context.fillText(`${x.toFixed(3)}, ${y.toFixed(3)}`, width / 2, height / 2);
+		/* in case you wanna use a square instead 
+		context.fillRect(
+			(width / 2) * (x + 1) - 5,
+			(height / 2) * (y + 1) - 5,
+			10,
+			10
+		);*/
+		context.beginPath();
+		context.arc(
+			(width / 2) * (x + 1),
+			(height / 2) * (-y + 1),
+			5,
+			0,
+			2 * Math.PI
+		);
+		context.fill();
+		context.strokeStyle = 'tomato';
+		context.beginPath();
+		context.moveTo(width / 2, height / 2);
+		context.lineTo((width / 2) * (x + 1), (height / 2) * (-y + 1));
+		context.stroke();
+				
+	};
 </script>
+
+<Controller bind:gamepad bind:output={gamepadOutput} />
+<Keyboard bind:output={keyboardOutput} />
 
 <div class="wrap">
 	<div class="grid">
@@ -10,20 +58,21 @@
 		<div class="item">
 			<p>test</p>
 		</div>
-		
 	</div>
 	<div class="bottomBar">
 		<div class="item">
-			<StickHandler stick="leftStick" />
+			<Canvas autoplay>
+				<Layer render={renderStick("leftStick")} />
+			</Canvas>
 		</div>
 		<div class="item">
-			<StickHandler stick="rightStick" />
+			<Canvas autoplay>
+				<Layer render={renderStick("rightStick")} />
+			</Canvas>
 		</div>
-		<div class="item">test</div>
 		<div class="item">test</div>
 	</div>
 </div>
-
 <style>
 	.item {
 		flex: 1;
@@ -45,4 +94,6 @@
 		min-height: 10vh;
 		background: var(--bgDark);
 	}
+
+	
 </style>
