84722d10bf778393215232cf362165d19d015c50
William Faircloth <willuhmjs@gmail.com>
2023-12-25 11:01:53 -0800

    feat: UA controls

--- apps/web/src/lib/components/handlers/Controller.svelte
+++ apps/web/src/lib/components/handlers/Controller.svelte
@@ -4,6 +4,7 @@
 
 	export let gamepad: Gamepad | null = null;
 	export let output: ControllerData;
+	const UA = navigator.userAgent;
 
 	onMount(() => {
 		function onGamepadConnected(e: GamepadEvent) {
@@ -27,6 +28,7 @@
 	function getInput() {
 		gamepad = navigator.getGamepads()[0];
 		if (!gamepad) return;
+		if (UA.includes("Linux")) {
 		output = {
 			connected: gamepad.connected,
 			stickAxes: {
@@ -52,13 +54,45 @@
 				right: gamepad.buttons[7].pressed
 			},
 			shapeButtons: {
-				up: gamepad.buttons[2].pressed,
 				down: gamepad.buttons[0].pressed,
+				right: gamepad.buttons[1].pressed,
+				up: gamepad.buttons[2].pressed,
 				left: gamepad.buttons[3].pressed,
-				right: gamepad.buttons[1].pressed
 			},
 		};
-
+	} else if (UA.includes("Windows")) {
+		output = {
+			connected: gamepad.connected,
+			stickAxes: {
+				leftStick: {
+					x: gamepad.axes[0],
+					y: gamepad.axes[1] * -1 // invert y axis (up is positive)
+				},
+				rightStick: {
+					x: gamepad.axes[2],
+					y: gamepad.axes[3] * -1 // invert y axis (up is positive)
+				}
+			},
+			stickButtons: {
+				leftStick: gamepad.buttons[10].pressed,
+				rightStick: gamepad.buttons[11].pressed
+			},
+			bumbers: {
+				left: gamepad.buttons[4].pressed,
+				right: gamepad.buttons[5].pressed
+			},
+			triggers: {
+				left: gamepad.buttons[6].pressed,
+				right: gamepad.buttons[7].pressed
+			},
+			shapeButtons: {
+				down: gamepad.buttons[0].pressed,
+				right: gamepad.buttons[1].pressed,
+				up: gamepad.buttons[3].pressed,
+				left: gamepad.buttons[2].pressed,
+			},
+		};
+	}
 		requestAnimationFrame(getInput);
 	}
 </script>
