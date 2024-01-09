c6a23a7a5efb1e131ac11f1a215b88c13f65b5a0
LeoDog896 <LeoDog896@hotmail.com>
2024-01-09 16:50:45 +0000

    refactor: move url resolving to cloud-url-resolver

--- apps/web/src/lib/connections/robot.ts
+++ apps/web/src/lib/connections/robot.ts
@@ -1,29 +1,7 @@
 import { createTRPCProxyClient, createWSClient, wsLink } from '@trpc/client';
 import type { RobotRouter } from 'robot/src/server';
 import { browser } from '$app/environment';
-
-type Protocol = 'http' | 'ws';
-
-function transform(port: number, protocol: Protocol = 'http'): string {
-	const resolvedProtocol =
-		location.protocol === 'https:' ? (protocol === 'http' ? 'https' : 'wss') : protocol;
-
-	if (browser) {
-		if (location.origin.includes('gitpod.io')) {
-			const url = location.origin.substring('https://xxxx'.length);
-
-			if (!url) throw Error('workspace url not defined');
-
-			const transformedURL = `${resolvedProtocol}://${port}${url}`;
-
-			return transformedURL;
-		}
-
-		// TODO: codespace support
-	}
-
-	return `${resolvedProtocol}://localhost:${port}`;
-}
+import { transform } from 'cloud-url-resolver';
 
 // TODO: transform this into a component
 export const client = browser
