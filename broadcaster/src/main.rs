dabf26c66ee8d2afdb21259ca048272945df0734
Yevgeny Coonan <yacoonan@gmail.com>
2024-04-16 19:13:52 +0000

    feat: opening and reading pixel data from a .jpeg image

--- broadcaster/src/main.rs
+++ broadcaster/src/main.rs
@@ -1,8 +1,21 @@
-use opencv::prelude::*;
+use std::io::Cursor;
+use image::io::Reader as ImageReader;
 
-fn main() -> Result<(),()>
+//#FF00F0 is the pink color used for testing.
+
+fn main()
 {
-    println!("Hello world!");
+    let image = ImageReader::open("/workspace/robot/broadcaster/pink.jpeg")
+        .expect("Failed to open file.")
+        .decode();
 
-    Ok(())
+    let rgb_image = image.unwrap().to_rgb8();
+
+    for (index,pixel) in rgb_image.pixels().enumerate()
+    {
+        for (val_i,rgb_value) in pixel.0.iter().enumerate()
+        {
+            println!("{}", rgb_value);
+        }
+    }
 }
