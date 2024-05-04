d5fb180b85302d773a49e2820cf248a3db9d469c
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-05-03 20:02:52 -0400

    refactor: rewrite video handling
    gosh dang it will

--- apps/video/src/index.ts
+++ apps/video/src/index.ts
@@ -1,10 +1,10 @@
 // Import child_process module
-import { exec } from 'child_process';
+import { exec } from 'node:child_process';
 const runCommand = 'ustreamer';
 
 // Function to check if uStreamer is installed
 const isUstreamerInstalled = () => {
-	const checkCommand = 'ustreamer --version';
+	const checkCommand = `${runCommand} --version`;
 	const checkProcess = exec(checkCommand);
 
 	return new Promise((resolve) => {
@@ -14,66 +14,70 @@ const isUstreamerInstalled = () => {
 	});
 };
 
-// Function to install uStreamer and run it
-const installAndRunUstreamer = async () => {
+async function run() {
+	const runProcess = exec(runCommand);
+
+	// Handle run process events
+	runProcess.stdout?.on('data', (data) => {
+		console.log(data.toString());
+	});
+
+	runProcess.stderr?.on('data', (data) => {
+		console.error(data.toString());
+	});
+
+	runProcess.on('close', (code) => {
+		console.log(`uStreamer process exited with code ${code}`);
+	});
+}
+
+async function install() {
+	console.log('uStreamer is not already installed. Attempting install...');
+
+	// Check if operating system is supported
+	const supportedOS = ['linux', 'darwin'];
+	const os = process.platform;
+
+	if (!supportedOS.includes(os)) {
+		console.error(`Unsupported operating system: ${os}.`);
+		console.error(`Perhaps you meant "pnpm run mock" instead?`);
+		return;
+	}
+
+	// Install uStreamer using apt-get
+	const installCommand = 'sudo apt-get install -y ustreamer';
+
+	// Execute the command
+	const installProcess = exec(installCommand);
+
+	// Handle install process events
+	installProcess.stdout?.on('data', (data) => {
+		console.log(`[INSTALL] ${data}`);
+	});
+
+	installProcess.stderr?.on('data', (data) => {
+		console.error(`[INSTALL ERROR] ${data}`);
+	});
+
+	installProcess.on('close', (code) => {
+		if (code === 0) {
+			run();
+		} else {
+			console.error('uStreamer installation failed.');
+		}
+	});
+}
+
+async function main() {
 	// Check if uStreamer is already installed
 	const isInstalled = await isUstreamerInstalled();
 
 	if (isInstalled) {
-		console.log('uStreamer is already installed. Skipping installation.');
-		const runProcess = exec(runCommand);
-
-		// Handle run process events
-		runProcess.stdout?.on('data', (data) => {
-			console.log(`[RUN] ${data}`);
-		});
-
-		runProcess.stderr?.on('data', (data) => {
-			console.error(`[RUN ERROR] ${data}`);
-		});
-
-		runProcess.on('close', (code) => {
-			console.log(`uStreamer process exited with code ${code}`);
-		});
+		await run()
 	} else {
-		console.log('uStreamer is not already installed. Attempting install...');
-		// Install uStreamer using apt-get
-		const installCommand = 'sudo apt-get install -y ustreamer';
-		// Execute the command
-		const installProcess = exec(installCommand);
-
-		// Handle install process events
-		installProcess.stdout?.on('data', (data) => {
-			console.log(`[INSTALL] ${data}`);
-		});
-
-		installProcess.stderr?.on('data', (data) => {
-			console.error(`[INSTALL ERROR] ${data}`);
-		});
-
-		installProcess.on('close', (code) => {
-			if (code === 0) {
-				// If installation is successful, execute the run command
-				const runProcess = exec(runCommand);
-
-				// Handle run process events
-				runProcess.stdout?.on('data', (data) => {
-					console.log(`[RUN] ${data}`);
-				});
-
-				runProcess.stderr?.on('data', (data) => {
-					console.error(`[RUN ERROR] ${data}`);
-				});
-
-				runProcess.on('close', (code) => {
-					console.log(`uStreamer process exited with code ${code}`);
-				});
-			} else {
-				console.error('uStreamer installation failed.');
-			}
-		});
+		await install();
 	}
 };
 
 // Call the function to install and run uStreamer
-installAndRunUstreamer();
+main();
