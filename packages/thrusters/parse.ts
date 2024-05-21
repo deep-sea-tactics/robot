f80121aae579ae9a39ea5b32f145b44b84d2dde5
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-05-20 21:50:21 -0400

    chore: begin data grabbing

--- packages/thrusters/parse.ts
+++ packages/thrusters/parse.ts
@@ -1,5 +1,5 @@
 import { readFile } from 'node:fs/promises';
-import { read } from 'xlsx';
+import { WorkSheet, read } from 'xlsx';
 
 const spreadsheet = await readFile('performance-data.xlsx');
 
@@ -9,4 +9,24 @@ const sheets = Object.fromEntries(
 	Object.entries(workbook.Sheets).filter(([key]) => key !== 'READ ME FIRST')
 );
 
-console.log(sheets)
+console.log(
+	Object.entries(sheets).map((a) =>
+		a.map((x, i) => {
+			if (i == 0) {
+				return x;
+			}
+
+			const sheet = x as WorkSheet;
+
+			return Object.fromEntries(
+				Object.entries(sheet)
+					// filtering for any actual cells (not any metadata)
+					.filter(([k]) => k == k.toUpperCase())
+					// selecting certain columns
+					.filter(([k]) => ['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(k[0]))
+					// trimming labels
+					.map(([k, v]) => [k, typeof v.v === 'string' ? v.v.trim() : v.v])
+			);
+		})
+	)
+);
