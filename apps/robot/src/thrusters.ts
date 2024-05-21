fd10e349a8f7bb0c97527ba6b282ccee9ff16c16
Tristan F. <26509014+LeoDog896@users.noreply.github.com>
2024-05-20 20:01:07 -0400

    feat: use linear algebra instead of gradient descent for motor calcs

--- apps/robot/src/thrusters.ts
+++ apps/robot/src/thrusters.ts
@@ -1,4 +1,4 @@
-import { gradientDescent } from './descent.js';
+import * as math from 'mathjs';
 import { Motor } from './motor.js';
 import * as vector from 'vector';
 
@@ -52,8 +52,6 @@ export const thrusters: MotorConstraint[] = [
 	}
 ];
 
-const orderedMotorTypes = thrusters.map((t) => t.type);
-
 interface MotorMovement {
 	type: Motor;
 	speed: number;
@@ -87,6 +85,48 @@ function calculateTorque(motorMovement: MotorMovement[]): vector.Vector {
 	}, vector.vector(0, 0, 0));
 }
 
+function calculateInverse() {
+	// Our control matrix
+	let controlMatrix = math.zeros([6, 6]);
+
+	const rovCenterOfMass = [0, 0, 0];
+
+	const thrusterValues = Object.values(thrusters);
+
+	// Populate the first three rows with force contributions
+    for (let i = 0; i < 6; i++) {
+        const orientation = math.matrix(vector.asTuple(thrusterValues[i].thrustDirection));
+		controlMatrix = math.subset(
+			controlMatrix,
+			math.index(math.range(0, 3), i),
+			math.reshape(orientation, [3, 1])
+		);
+    }
+
+    // Populate the last three rows with torque contributions
+    for (let i = 0; i < 6; i++) {
+        const location = vector.asTuple(thrusterValues[i].position);
+        const orientation = vector.asTuple(thrusterValues[i].thrustDirection);
+        const displacement = math.subtract(location, rovCenterOfMass);
+		// using cross product here to account for the extra rotation produced from
+		// the motors
+        const torque = math.reshape(math.transpose(math.cross(displacement, orientation)), [3, 1]);
+        controlMatrix = math.subset(controlMatrix, math.index(math.range(3, 6), i), torque);
+    }
+
+    // Calculate the inverse of the control matrix, so we can map the motors to force and torque,
+	// instead of the other way around.
+    return math.inv(controlMatrix);
+}
+
+const controlInverse = calculateInverse();
+
+function convertToMotorPowers(force: vector.VectorTuple, torque: vector.VectorTuple) {
+	const inputVector = math.transpose(math.matrix([...force, ...torque]));
+	const motorPowers = math.multiply(controlInverse, inputVector);
+	return motorPowers;
+}
+
 /**
  * Gives the optimal configuration
  * to direct the robot in the given direction.
@@ -98,44 +138,26 @@ function calculateTorque(motorMovement: MotorMovement[]): vector.Vector {
  * This function will compromise evenly between direction and torque, getting the smallest difference on both.
  */
 export function move(direction: vector.Vector, torque: vector.Vector): MoveOutput {
-	// we can abstract this problem as to finding the local minima of a function with 6 numerical inputs clamped to [-1, 1]
-	// if we say f(a, b, c, d, e, g) = (directionDifference(a, b, c, d, e, g) + torqueDifference(a, b, c, d, e, g)) / 2
-	// we can use a gradient descent algorithm to find the local minima
+	// Convert the combined vector to motor powers using the control matrix inverse
+	const motorPowers = convertToMotorPowers(vector.asTuple(direction), vector.asTuple(torque));
 
-	const outputs = gradientDescent(
-		(a, b, c, d, e, g) => {
-			const motorMovement: MotorMovement[] = [
-				{ type: Motor.BottomLeft, speed: a },
-				{ type: Motor.BottomRight, speed: b },
-				{ type: Motor.TopLeft, speed: c },
-				{ type: Motor.TopRight, speed: d },
-				{ type: Motor.VerticalLeft, speed: e },
-				{ type: Motor.VerticalRight, speed: g }
-			];
-
-			const directionDifference = vector.magnitude(
-				vector.subtract(direction)(calculateForce(motorMovement))
-			);
-			const torqueDifference = vector.magnitude(
-				vector.subtract(torque)(calculateTorque(motorMovement))
-			);
-
-			return (directionDifference + torqueDifference) / 2;
-		},
-		[0, 0, 0, 0, 0, 0]
-	);
-
-	const motorMovement: MotorMovement[] = [
-		{ type: Motor.BottomLeft, speed: outputs[0] },
-		{ type: Motor.BottomRight, speed: outputs[1] },
-		{ type: Motor.TopLeft, speed: outputs[2] },
-		{ type: Motor.TopRight, speed: outputs[3] },
-		{ type: Motor.VerticalLeft, speed: outputs[4] },
-		{ type: Motor.VerticalRight, speed: outputs[5] }
+	// Map the motor powers to the corresponding thruster types
+	const motorMovement = [
+		{ type: Motor.BottomLeft, speed: motorPowers.get([0]) },
+		{ type: Motor.BottomRight, speed: motorPowers.get([1]) },
+		{ type: Motor.TopLeft, speed: motorPowers.get([2]) },
+		{ type: Motor.TopRight, speed: motorPowers.get([3]) },
+		{ type: Motor.VerticalLeft, speed: motorPowers.get([4]) },
+		{ type: Motor.VerticalRight, speed: motorPowers.get([5]) }
 	];
 
-	const directionDifference = vector.subtract(direction)(calculateForce(motorMovement));
-	const torqueDifference = vector.subtract(torque)(calculateTorque(motorMovement));
+	// Calculate the resulting force and torque for the given motor speeds
+	const resultingForce = calculateForce(motorMovement);
+	const resultingTorque = calculateTorque(motorMovement);
+
+	// Compute the differences between the desired and resulting force and torque
+	const directionDifference = vector.subtract(direction)(resultingForce);
+	const torqueDifference = vector.subtract(torque)(resultingTorque);
 
 	return {
 		motors: motorMovement,
