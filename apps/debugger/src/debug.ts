8b36475fb0cecff53152d23006eeb782d73d7781
Tristan F. <LeoDog896@hotmail.com>
2024-05-29 11:59:41 -0400

    chore(debugger): rewrite for better err handling and typing

--- apps/debugger/src/debug.ts
+++ apps/debugger/src/debug.ts
@@ -1,15 +1,4 @@
-import { Gpio } from 'pigpio';
-import { program } from 'commander';
-
-program
-	.option('-p, --pin <pin>', 'Pin number')
-	.option('-d, --digital', 'Digital write')
-	.option('-s, --servo', 'Servo write')
-	.option('-a, --analog', 'PWM/analog write')
-	.option('-i, --info', 'Get info about a GPIO device.')
-	.option('-v, --value <value>', 'Value to write');
-
-program.parse(process.argv);
+import { program } from '@commander-js/extra-typings';
 
 function getOrTry<T>(f: () => T): T | null {
 	try {
@@ -19,41 +8,47 @@ function getOrTry<T>(f: () => T): T | null {
 	}
 }
 
-const { pin, digital, servo, value, analog, info } = program.opts();
+program
+	.name('pigpio-cli')
+	.description('PIGPIO CLI debugging tool.')
+	.usage('pigpio-cli --pin <pin> (--digital/--servo/--analog) --value <value>')
+	.requiredOption('-p, --pin <pin>', 'Pin number')
+	.option('-d, --digital', 'Digital write')
+	.option('-s, --servo', 'Servo write')
+	.option('-a, --analog', 'PWM/analog write')
+	.option('-i, --info', 'Get info about a GPIO device.')
+	.option('-v, --value <value>', 'Value to write')
+	.showHelpAfterError(true)
+	.action(async ({ pin, digital, servo, analog, info, value }) => {
+		const { Gpio } = await import('pigpio');
 
-if (!pin) {
-	console.error('Pin number is required');
-	process.exit(1);
-}
+		const gpio = new Gpio(parseInt(pin), { mode: Gpio.OUTPUT });
 
-const gpio = new Gpio(parseInt(pin), { mode: Gpio.OUTPUT });
+		if (info) {
+			console.log("PWM Duty Cycle:", getOrTry(gpio.getPwmDutyCycle) ?? "Could not get.");
+			console.log("PWM Frequency:", getOrTry(gpio.getPwmFrequency) ?? "Could not get.");
+			console.log("PWM Range:", getOrTry(gpio.getPwmRange) ?? "Could not get.");
+			console.log("PWM Real Range:", getOrTry(gpio.getPwmRealRange) ?? "Could not get.");
+			console.log("Servo Pulse Width:", getOrTry(gpio.getServoPulseWidth) ?? "Could not get.");
+			return;
+		}
 
-if (info) {
-	console.log("PWM Duty Cycle", getOrTry(gpio.getPwmDutyCycle) ?? "Could not get.");
-	console.log("PWM Frequency", getOrTry(gpio.getPwmFrequency) ?? "Could not get.");
-	console.log("PWM Range", getOrTry(gpio.getPwmRange) ?? "Could not get.");
-	console.log("PWM Real Range", getOrTry(gpio.getPwmRealRange) ?? "Could not get.");
-	console.log("Servo Pulse Width", getOrTry(gpio.getServoPulseWidth) ?? "Could not get.");
-	process.exit(0);
-}
+		if (!value) {
+			program.error('error: required option, \'-v, --value <value>\' not specified');
+		}
 
-if (!value) {
-	console.error('Value is required');
-	process.exit(1);
-}
+		if ([digital, servo, analog].filter(Boolean).length > 1) {
+			program.error('Only either --digital, --servo, or --analog (pwm) must be specified');
+		}
 
-if ([digital, servo, analog].filter(Boolean).length > 1) {
-	console.error('Only either --digital or --servo must be specified');
-	process.exit(1);
-}
-
-if (digital) {
-	gpio.digitalWrite(parseInt(value));
-} else if (servo) {
-	gpio.servoWrite(parseInt(value));
-} else if (analog) {
-	gpio.pwmWrite(parseInt(value));
-} else {
-	console.error('Either --digital or --servo must be specified');
-	process.exit(1);
-}
+		if (digital) {
+			gpio.digitalWrite(parseInt(value));
+		} else if (servo) {
+			gpio.servoWrite(parseInt(value));
+		} else if (analog) {
+			gpio.pwmWrite(parseInt(value));
+		} else {
+			program.error('Either --digital, --servo, or --analog (pwm) must be specified');
+		}
+	})
+	.parse();
