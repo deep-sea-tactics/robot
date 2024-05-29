00848c35e842450041fb25f6fa522c98ab3354d2
Tristan F. <LeoDog896@hotmail.com>
2024-05-29 07:55:21 -0400

    feat: add info and analog/pwm write to debugger

--- apps/debugger/src/debug.ts
+++ apps/debugger/src/debug.ts
@@ -5,11 +5,13 @@ program
 	.option('-p, --pin <pin>', 'Pin number')
 	.option('-d, --digital', 'Digital write')
 	.option('-s, --servo', 'Servo write')
+	.option('-p, --pwm', 'PWM/analog write')
+	.option('-i, --info', 'Get info about a GPIO device.')
 	.option('-v, --value <value>', 'Value to write');
 
 program.parse(process.argv);
 
-const { pin, digital, servo, value } = program.opts();
+const { pin, digital, servo, value, pwm, info } = program.opts();
 
 if (!pin) {
 	console.error('Pin number is required');
@@ -18,12 +20,21 @@ if (!pin) {
 
 const gpio = new Gpio(parseInt(pin), { mode: Gpio.OUTPUT });
 
+if (info) {
+	console.log("PWM Duty Cycle", gpio.getPwmDutyCycle());
+	console.log("PWM Frequency", gpio.getPwmFrequency());
+	console.log("PWM Range", gpio.getPwmRange());
+	console.log("PWM Real Range", gpio.getPwmRealRange());
+	console.log("Servo Pulse Width", gpio.getServoPulseWidth());
+	process.exit(0);
+}
+
 if (!value) {
 	console.error('Value is required');
 	process.exit(1);
 }
 
-if (digital && servo) {
+if ([digital, servo, pwm].filter(Boolean).length > 1) {
 	console.error('Only either --digital or --servo must be specified');
 	process.exit(1);
 }
@@ -32,6 +43,8 @@ if (digital) {
 	gpio.digitalWrite(parseInt(value));
 } else if (servo) {
 	gpio.servoWrite(parseInt(value));
+} else if (pwm) {
+	gpio.pwmWrite(parseInt(value));
 } else {
 	console.error('Either --digital or --servo must be specified');
 	process.exit(1);
