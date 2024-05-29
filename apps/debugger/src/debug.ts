20a80db60ff4d9414ab8696cce682ef46bb988b1
Tristan F. <LeoDog896@hotmail.com>
2024-05-29 07:56:54 -0400

    fix: switch naming to analog, a

--- apps/debugger/src/debug.ts
+++ apps/debugger/src/debug.ts
@@ -5,13 +5,13 @@ program
 	.option('-p, --pin <pin>', 'Pin number')
 	.option('-d, --digital', 'Digital write')
 	.option('-s, --servo', 'Servo write')
-	.option('-p, --pwm', 'PWM/analog write')
+	.option('-a, --analog', 'PWM/analog write')
 	.option('-i, --info', 'Get info about a GPIO device.')
 	.option('-v, --value <value>', 'Value to write');
 
 program.parse(process.argv);
 
-const { pin, digital, servo, value, pwm, info } = program.opts();
+const { pin, digital, servo, value, analog, info } = program.opts();
 
 if (!pin) {
 	console.error('Pin number is required');
@@ -34,7 +34,7 @@ if (!value) {
 	process.exit(1);
 }
 
-if ([digital, servo, pwm].filter(Boolean).length > 1) {
+if ([digital, servo, analog].filter(Boolean).length > 1) {
 	console.error('Only either --digital or --servo must be specified');
 	process.exit(1);
 }
@@ -43,7 +43,7 @@ if (digital) {
 	gpio.digitalWrite(parseInt(value));
 } else if (servo) {
 	gpio.servoWrite(parseInt(value));
-} else if (pwm) {
+} else if (analog) {
 	gpio.pwmWrite(parseInt(value));
 } else {
 	console.error('Either --digital or --servo must be specified');
