2819aacf8b4ab8f55daa90bc9cfd483d01d5f424
Kiwibiwii <Cdaniela2006@gmail.com>
2024-06-05 20:32:44 -0400

    fix: reconfigure thrusters

--- apps/robot/src/thrusters.ts
+++ apps/robot/src/thrusters.ts
@@ -16,39 +16,39 @@ const thrustOffset = -30;
 export const thrusters: MotorConstraint[] = [
 	{
 		type: Motor.BottomLeft,
-		position: vector.vector(85.075 / 1000, -67.57 / 1000, 95.417 / 1000),
+		position: vector.vector(-95.233 / 1000, -18.314 / 1000, -119.28 / 1000),
 		maxThrust: t200_12v_max_newtons + thrustOffset,
-		thrustDirection: vector.vector(-1, 0, 1)
+		thrustDirection: vector.normalize(vector.vector(2, 0, -1))
 	},
 	{
 		type: Motor.BottomRight,
-		position: vector.vector(85.075 / 1000, -67.57 / 1000, -95.417 / 1000),
+		position: vector.vector(-95.233 / 1000, -18.314 / 1000, 119.28 / 1000),
 		maxThrust: t200_12v_max_newtons + thrustOffset,
-		thrustDirection: vector.vector(-1, 0, -1)
+		thrustDirection: vector.normalize(vector.vector(2, 0, 1))
 	},
 	{
 		type: Motor.TopLeft,
-		position: vector.vector(-114.204 / 1000, 65.297 / 1000, 103.873 / 1000),
+		position: vector.vector(105.808 / 1000, 65.297 / 1000, -112.299 / 1000),
 		maxThrust: t200_12v_max_newtons + thrustOffset,
-		thrustDirection: vector.vector(1, 0, 1)
+		thrustDirection: vector.normalize(vector.vector(2, 0, 1))
 	},
 	{
 		type: Motor.TopRight,
-		position: vector.vector(-114.204 / 1000, 65.297 / 1000, -103.873 / 1000),
+		position: vector.vector(105.808 / 1000, 41.738 / 1000, 112.299 / 1000),
 		maxThrust: t200_12v_max_newtons + thrustOffset,
-		thrustDirection: vector.vector(1, 0, -1)
+		thrustDirection: vector.normalize(vector.vector(2, 0, -1))
 	},
 	{
 		type: Motor.VerticalLeft,
-		position: vector.vector(20.755 / 1000, 80.601 / 1000, 99.732 / 1000),
+		position: vector.vector(2.231 / 1000, -66.776 / 1000, -119.862 / 1000),
 		maxThrust: t200_12v_max_newtons + thrustOffset,
-		thrustDirection: vector.vector(0, 1, 0)
+		thrustDirection: vector.normalize(vector.vector(0, 1, 0))
 	},
 	{
 		type: Motor.VerticalRight,
-		position: vector.vector(20.755 / 1000, 80.601 / 1000, -99.732 / 1000),
+		position: vector.vector(2.231 / 1000, -66.776 / 1000, 119.862 / 1000),
 		maxThrust: t200_12v_max_newtons + thrustOffset,
-		thrustDirection: vector.vector(0, 1, 0)
+		thrustDirection: vector.normalize(vector.vector(0, 1, 0))
 	}
 ];
 
