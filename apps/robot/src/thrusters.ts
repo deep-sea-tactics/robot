7b3f1fe34d64c8f0dc8b72274b6653dda3171834
Kiwibiwii <Cdaniela2006@gmail.com>
2024-06-05 20:58:00 -0400

    debug: log

--- apps/robot/src/thrusters.ts
+++ apps/robot/src/thrusters.ts
@@ -145,7 +145,7 @@ function convertToMotorPowers(force: vector.VectorTuple, torque: vector.VectorTu
 		motorPowers.get([5]) ** 2
 	);
 	
-	console.log(magnitude, motorPowers, forceMax, torqueMax)
+	console.log(force, torque)
 	return motorPowers.map(x => (magnitude == 0 ? 0 : (x / magnitude)) * maxEither);
 }
 
