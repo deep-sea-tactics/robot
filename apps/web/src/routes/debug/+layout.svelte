f1c22f8f9971b6b7324b9d19b3c99965e6dad1c7
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-06-11 21:38:58 -0400

    feat(debugger): make it a little fancier

--- apps/web/src/routes/debug/+layout.svelte
+++ apps/web/src/routes/debug/+layout.svelte
@@ -1,11 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import PhWrenchFill from '~icons/ph/wrench-fill';
+</script>
+
 <div class="container">
 	<header>
-		<h1>Debugging</h1>
+		<h1>ROV Debugger <PhWrenchFill /></h1>
 
 		<div class="links">
-			<a href="/debug/thruster">Thruster</a>
-			<a href="/debug/controller">Controller</a>
-			<a href="/debug/control-matrix">Control Matrix</a>
+			<a class:active={$page.url.toString().includes('/debug/thruster')} href="/debug/thruster">Thruster</a>
+			<a class:active={$page.url.toString().includes('/debug/controller')} href="/debug/controller">Controller</a>
+			<a class:active={$page.url.toString().includes('/debug/control-matrix')} href="/debug/control-matrix">Control Matrix</a>
 		</div>
 	</header>
 
@@ -27,8 +32,8 @@
 	}
 
 	:global(html, body) {
-		height: calc(100% - 2rem);
-		margin: 1rem;
+		height: 100%;
+		margin: 0;
 		padding: 0;
 	}
 
@@ -39,6 +44,15 @@
 		}
 	}
 
+	.active {
+		background-color: var(--accent);
+	}
+
+	h1 {
+		display: flex;
+		gap: 1rem;
+	}
+
 	.container {
 		width: 100%;
 		height: 100%;
@@ -47,14 +61,23 @@
 	}
 
 	.body {
-		width: 100%;
-		height: 100%;
+		width: calc(100% - 2rem);
+		height: calc(100% - 2rem);
+		margin: 1rem;
+	}
+
+	.links {
+		display: flex;
+		gap: 1rem;
 	}
 
 	header {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		align-items: center;
+		justify-content: space-between;
+		padding: 1rem;
+		border-bottom: 1px solid var(--foreground);
 
 		a {
 			background-color: var(--foreground);
