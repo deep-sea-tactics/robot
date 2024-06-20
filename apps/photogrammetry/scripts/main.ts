b8de715afe2cc4ea0c3ef9352ebc1d8cdaba2a20
Tristan F. <LeoDog896@hotmail.com>
2024-06-20 01:01:59 -0400

    feat: copy output over to project/images

--- apps/photogrammetry/scripts/main.ts
+++ apps/photogrammetry/scripts/main.ts
@@ -2,7 +2,7 @@ import { path } from '@ffmpeg-installer/ffmpeg';
 import ffmpeg from 'fluent-ffmpeg';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'node:path';
-import { readdir, mkdir } from 'fs/promises';
+import { readdir, mkdir, copyFile } from 'fs/promises';
 
 const currentDirectory = dirname(fileURLToPath(import.meta.url));
 
@@ -10,12 +10,13 @@ ffmpeg.setFfmpegPath(path);
 
 const videos = await readdir(join(currentDirectory, '..', 'videos'));
 
-await mkdir(join(currentDirectory, '..', 'output'))
+await mkdir(join(currentDirectory, '..', 'output'), { recursive: true });
+await mkdir(join(currentDirectory, '..', 'project', 'images'), { recursive: true });
 
 for (let i = 0; i < videos.length; i++) {
 	const video = videos[i];
 
-	const command = ffmpeg()
+	ffmpeg()
 		.input(join(currentDirectory, '..', 'videos', video))
 		.outputOptions('-vf', 'fps=5')
 		.on('end', () => {
@@ -26,3 +27,14 @@ for (let i = 0; i < videos.length; i++) {
 		})
 		.save(join(currentDirectory, '..', 'output') + `/frame-v${i}-%03d.png`);
 }
+
+// copy the files over in output to project
+const outputFiles = await readdir(join(currentDirectory, '..', 'output'));
+
+for (const file of outputFiles) {
+	const srcPath = join(currentDirectory, '..', 'output', file);
+	const destPath = join(currentDirectory, '..', 'project', 'images', file);
+	await copyFile(srcPath, destPath);
+}
+
+console.log('All frames copied to project/images.');
