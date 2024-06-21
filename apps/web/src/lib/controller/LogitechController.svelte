ad127e91a1f40a4c396ed5df86fb901aaf2421ce
Tristan F. <LeoDog896@hotmail.com>
2024-06-21 07:52:53 -0400

    fix: controller logitech deadzone

--- apps/web/src/lib/controller/LogitechController.svelte
+++ apps/web/src/lib/controller/LogitechController.svelte
@@ -13,16 +13,16 @@
 		connected: gamepad.connected,
 		id: gamepad.id,
 		movement: {
-			x: gamepad.axes[0] * -1,
-			z: gamepad.axes[1] * -1,
+			x: deadzone(gamepad.axes[0] * -1, [-0.02, 0.02]),
+			z: deadzone(gamepad.axes[1] * -1, [-0.02, 0.02]),
 			y: buttonAxis(gamepad.buttons[12].pressed, gamepad.buttons[13].pressed)
 		},
 		rotation: {
-			pitch: gamepad.axes[2],
-			yaw: gamepad.axes[3] * -1
+			yaw: buttonAxis(gamepad.buttons[14].pressed, gamepad.buttons[15].pressed),
+			pitch: deadzone(gamepad.axes[3] * -1, [-0.02, 0.02])
 		},
 		tasks: {
-			pinkSquare: gamepad.buttons[0].pressed,
+			pinkSquare: gamepad.buttons[3].pressed,
 			scanning: gamepad.buttons[1].pressed
 		},
 		camera: {
@@ -30,7 +30,7 @@
 		},
 		arm: {
 			openClose: (gamepad.axes[5] + 1) / 2 - (gamepad.axes[4] + 1) / 2,
-			rotate: buttonAxis(gamepad.buttons[9].pressed, gamepad.buttons[8].pressed)
+			rotate: buttonAxis(gamepad.buttons[4].pressed, gamepad.buttons[5].pressed)
 		}
 	})}
 />
